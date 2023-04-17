b41aa80c14bf9e01770d3ea832f771598d76be2c
Matteo Bunino <matteo.bunino@gmail.com>
2023-04-17 09:57:12 +0200

    Fix typo

--- examples.sh
+++ examples.sh
@@ -4,4 +4,4 @@
 conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/training-workflow.yml
 
 # MNIST inference
-conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/inferece-workflow.yml
+conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/inference-workflow.yml
