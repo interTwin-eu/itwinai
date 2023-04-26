e20f5c99c07835c4bdd0973efebe6d2e722f5376
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2023-04-26 10:21:23 +0200

    AI v0.0 (#16)
    * Add MNIST preproc
    
    * Add GH files
    
    * Fix README
    
    * Train function CLI
    
    * Update env files
    
    * Update environment files
    
    * Add GH actions
    
    * Fix format
    
    * Update linter
    
    * Update linter
    
    * Update linter
    
    * Refactor
    
    ---------
    
    Co-authored-by: Matteo Bunino <matteo.bunino@cern.ch>

--- ai/README.md
+++ ai/README.md
@@ -1,1 +1,20 @@
 # AI workflows (T6.5)
+
+## Development env setup
+
+Requirements:
+
+- Mamba / conda
+
+Installation:
+
+```bash
+# Optional
+cd ./ai
+# Inside ./ai folder
+mamba env create -p ./.venv --file ./dev-env.yml
+# Install this package
+conda run -p ./.venv python -m pip install --no-deps -e .
+# Activate env
+conda activate ./.venv
+```
---
+++ ai/src/itwinai/utils.py
@@ -0,0 +1,20 @@
+"""
+Utilities for itwinai package.
+"""
+
+
+def dynamically_import_class(name: str):
+    """
+    Dynamically import class by module path.
+    Adapted from https://stackoverflow.com/a/547867
+
+    Args:
+        name (str): path to the class (e.g., mypackage.mymodule.MyClass)
+
+    Returns:
+        __class__: class object.
+    """
+    module, class_name = name.rsplit(".", 1)
+    mod = __import__(module, fromlist=[class_name])
+    klass = getattr(mod, class_name)
+    return klass
