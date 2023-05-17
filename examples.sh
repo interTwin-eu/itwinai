60c3b6be6a6f892abb7180279a9d606e4e15e5ac
Matteo Bunino <matteo.bunino@cern.ch>
2023-05-17 09:09:23 +0200

    Reorg and add headings

--- examples.sh
+++ examples.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 # Launch some workflows
 
+################################
+###    Training examples     ###
+################################
+
 # MNIST training
 conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/training-workflow.yml
+
+# MNIST training (with CWL workflow definition)
+conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/training-workflow.yml --cwl
+
+# AI commnd: from within AI venv
+itwinai train --input ./data/mnist/preproc-images-train --output ./data/mnist/ml-logs --config ./use-cases/mnist/mnist-ai.yml
+
+################################
+###   Inference examples     ###
+################################
 
 # MNIST inference
 conda run -p ./.venv python run-workflow.py -f ./use-cases/mnist/inference-workflow.yml
 
-# AI commnd: from within AI venv
-itwinai train --input ./data/mnist/preproc-images-train --output ./data/mnist/ml-logs --config ./use-cases/mnist/mnist-ai.yml
+################################
+###  Visualization examples  ###
+################################
 
 # Visualize logs
 conda activate ./ai/.venv-dev && itwinai visualize --path ./data/mnist/ml-logs
