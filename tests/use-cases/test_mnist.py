d738562de17a9377271e7774a834be9d16504e56
Matteo Bunino <matteo.bunino@cern.ch>
2023-05-19 13:33:19 +0200

    Add tests

---
+++ tests/use-cases/test_mnist.py
@@ -0,0 +1,14 @@
+"""
+Tests for MNIST use case.
+
+Intended to be integration tests, to make sure that updates in the code base
+do not break use cases' workflows.
+"""
+
+import pytest
+
+
+@pytest.mark.integration
+def test_training_workflow():
+    # TODO: complete
+    assert 1 == 1
