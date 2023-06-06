230922851d5fa173371f86ae53f957e8bcfc2ebe
r-sarma <126173968+r-sarma@users.noreply.github.com>
2023-06-06 15:35:36 +0530

    Update mnist-preproc.py

--- use-cases/mnist/mnist-preproc.py
+++ use-cases/mnist/mnist-preproc.py
@@ -20,8 +20,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    #args.output = "preproc-images"
-
     if args.stage == 'train':
         # Download and store training dataset
         MNIST(args.output, train=True, download=True)
