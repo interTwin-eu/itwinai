752a1987ee18d269836cc0d30513839241f24dc8
Alexander Zoechbauer <azoechba@singularity-vm.cern.ch>
2023-07-04 16:06:00 +0200

    ported AI and preprocessing module to singularity

--- ai/containers/build-base-container.sh
+++ ai/containers/build-base-container.sh
@@ -3,7 +3,14 @@
 IMAGE_NAME="$1"
 
 if [ -z "$IMAGE_NAME" ]; then
+<<<<<<< HEAD
     IMAGE_NAME=ai/containers/base-container.sif
 fi
 
 singularity build $IMAGE_NAME ai/containers/base-container.def
+=======
+    IMAGE_NAME=base-container.sif
+fi
+
+singularity build $IMAGE_NAME base-container.def
+>>>>>>> 00abab4 (ported AI and preprocessing module to singularity)
--- use-cases/mnist/containers/build-preproc-container.sh
+++ use-cases/mnist/containers/build-preproc-container.sh
@@ -3,7 +3,14 @@
 IMAGE_NAME="$1"
 
 if [ -z "$IMAGE_NAME" ]; then
+<<<<<<< HEAD
     IMAGE_NAME=use-cases/mnist/containers/preproc-container.sif
 fi
 
 singularity build $IMAGE_NAME use-cases/mnist/containers/preproc-container.def
+=======
+    IMAGE_NAME=preproc-container.sif
+fi
+
+singularity build $IMAGE_NAME preproc-container.def
+>>>>>>> 00abab4 (ported AI and preprocessing module to singularity)
