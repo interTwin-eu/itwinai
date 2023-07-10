246d37fda003d66742b2a4a73aa55de4fd19354e
linxUser3574 <neonikkus@gmail.com>
2023-07-10 09:13:33 +0000

    FIX: Paths and downloading of the data

--- use-cases/tensorflow/cyclones/dataloader.py
+++ use-cases/tensorflow/cyclones/dataloader.py
@@ -5,11 +5,11 @@ import argparse
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='trainval.py', description="Train a VGG custom model on eFlows Dataset")
+    parser = argparse.ArgumentParser()
 
     # REQUIRED ARGUMENTS
-    parser.add_argument("-o", "--output_dir", type=int, help="Output directory", required=True)
+    parser.add_argument("-o", "--output_dir", type=str, help="Output directory", required=True)
     args = parser.parse_args()
 
     url = 'https://drive.google.com/drive/folders/15DEq33MmtRvIpe2bNCg44lnfvEiHcPaf'
-    gdown.download_folder(url=url, quiet=False)
+    gdown.download_folder(url=url, quiet=False, output=args.output_dir)
