1b0065ca3fa8d529f9aeb9ef3216f6f21516dd24
User3574 <neonikkus@gmail.com>
2023-08-09 15:14:54 -0400

    FIX: Distributed Dataset

--- use-cases/zebra2horse/train.py
+++ use-cases/zebra2horse/train.py
@@ -2,7 +2,9 @@ import argparse
 
 from trainer import Zebra2HorseTrainer
 from dataloader import Zebra2HorseDataLoader
-from itwinai.backend.executors import LocalExecutor
+from itwinai.backend.executors import LocalExecutor, RayExecutor
+from ray import tune
+
 
 if __name__ == "__main__":
     # Create CLI Parser
@@ -11,9 +13,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Execute pipe
-    executor = LocalExecutor(pipeline=args.pipeline, class_dict={
+    executor = RayExecutor(pipeline=args.pipeline, class_dict={
         "loader": Zebra2HorseDataLoader,
         "trainer": Zebra2HorseTrainer
+    }, param_space={
+        "trainer": {
+            "init_args": {
+                "batch_size": tune.randint(32, 512),
+            }
+        }
     })
     executor.setup(None)
     executor.execute(None)
