67e4da94ab473e06889c0d8a2a917f630024e7d5
Alexander Zoechbauer <zoechbauer1@hdfmll01.hdfml>
2023-10-17 17:09:23 +0200

    Container updated based on NGC for HDFML

---
+++ containers/apptainer/apptainer_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# version: 2212008a
+# pull and build containers for PyTorch/NVIDIA
+
+# load modules
+ml GCC/11.3.0 OpenMPI/4.1.4 cuDNN/8.6.0.163-CUDA-11.7 Apptainer-Tools/2023
+
+# create Cache/TMP so that $HOME would not be used
+mkdir -p Cache
+mkdir -p TMP 
+export APPTAINER_CACHEDIR=$(mktemp -d -p $PWD/Cache)
+export APPTAINER_TMPDIR=$(mktemp -d -p $PWD/TMP)
+
+# official NVIDIA NVCR container with Torch==2.0.0
+apptainer pull torch.sif docker://nvcr.io/nvidia/pytorch:23.09-py3
+
+# run bash to create envs
+echo "running ./apptainer_build_env.sh"
+apptainer exec torch.sif bash -c "./apptainer_build_env.sh"
+
+#eof
---
+++ use-cases/mnist/torch/startscript.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# general configuration of the job
+#SBATCH --job-name=itwinaiTraining
+#SBATCH --account=intertwin
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+#SBATCH --time=00:30:00
+
+# configure node and process count on the CM
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-node=4
+
+#SBATCH --exclusive
+
+# gres options have to be disabled for deepv
+#SBATCH --gres=gpu:4
+
+# parameters
+debug=false # display debug info
+
+WORKDIR="/p/project/intertwin/zoechbauer1/T6.5-AI-and-ML/use-cases/mnist/torch"
+CONTAINERPATH="/p/project/intertwin/zoechbauer1/T6.5-AI-and-ML/containers/apptainer/torch.sif"
+
+#EXEC="python train.py -p pipeline.yaml --download-only" #for bash
+EXEC="python train.py -p pipeline.yaml"                 #for SLURM
+
+SLURM_EXECUTION=true
+
+
+### DO NOT MODIFY THE SCRIPT BELOW THIS LINE ###
+
+# set modules and envs
+ml GCC/11.3.0 OpenMPI/4.1.4 cuDNN/8.6.0.163-CUDA-11.7 Apptainer-Tools/2023
+# source $SLURM_SUBMIT_DIR/torch_env/bin/activate
+
+# set env vars
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export OMP_NUM_THREADS=1
+if [ "$SLURM_CPUS_PER_TASK" > 0 ] ; then
+  export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+fi
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+
+# job info 
+echo "DEBUG: TIME: $(date)"
+echo "DEBUG: EXECUTE: $EXEC"
+echo "DEBUG: SLURM_SUBMIT_DIR: $SLURM_SUBMIT_DIR"
+echo "DEBUG: SLURM_JOB_ID: $SLURM_JOB_ID"
+echo "DEBUG: SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
+echo "DEBUG: SLURM_NNODES: $SLURM_NNODES"
+echo "DEBUG: SLURM_NTASKS: $SLURM_NTASKS"
+echo "DEBUG: SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
+echo "DEBUG: SLURM_GPUS_PER_NODE: $SLURM_GPUS_PER_NODE" 
+echo "DEBUG: SLURM_SUBMIT_HOST: $SLURM_SUBMIT_HOST"
+echo "DEBUG: SLURMD_NODENAME: $SLURMD_NODENAME"
+echo "DEBUG: SLURM_NODEID: $SLURM_NODEID"
+echo "DEBUG: CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+if [ "$debug" = true ] ; then
+  export NCCL_DEBUG=INFO
+fi
+
+#Choose SLURM execution or bash script execution
+if [ "$SLURM_EXECUTION" = true ]; then
+    srun --cpu-bind=none bash -c "apptainer exec --nv \
+        --workdir=$WORKDIR\
+        $CONTAINERPATH \
+        $EXEC"
+else
+    apptainer exec --nv \
+        --workdir=$WORKDIR\
+        $CONTAINERPATH \
+        $EXEC
+fi
+
+#eof
+
+
+
