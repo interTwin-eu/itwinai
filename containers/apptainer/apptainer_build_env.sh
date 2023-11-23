848ff2139839f24a0e33df27880b579a14c4dae4
Alexander Zoechbauer <azoechba@singularity-vm.cern.ch>
2023-11-23 15:24:39 +0100

    removed GH action, removed comments from apptainer_build_env.sh

--- containers/apptainer/apptainer_build_env.sh
+++ containers/apptainer/apptainer_build_env.sh
@@ -1,9 +1,4 @@
 nname='torch_env'
 
 # create env inside container
-# python3 -m venv $nname --system-site-packages
-# source ${nname}/bin/activate
-
-# install wheels -- from this point on, feel free to add anything
-#pip3 install -r ./env-files/torch/pytorch-env-gpu-container.txt
 pip3 install -e .[dev]
