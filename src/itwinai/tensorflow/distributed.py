2caa42307a8d924a05d3eaf53ea31d604f37a473
r-sarma <126173968+r-sarma@users.noreply.github.com>
2024-02-20 18:31:42 +0530

    Update distributed.py

--- src/itwinai/tensorflow/distributed.py
+++ src/itwinai/tensorflow/distributed.py
@@ -1,7 +1,14 @@
-from ..distributed import DistributedStrategy
+import tensorflow as tf
 
+def get_strategy():
+    """Strategy for distributed TensorFlow training"""
+    implementation = tf.distribute.experimental.CommunicationImplementation.NCCL
+    communication_options = tf.distribute.experimental.CommunicationOptions(implementation=implementation)
 
-class TFDistributedStrategy(DistributedStrategy):
-    """Abstract class to define the distributed backend methods for
-    TensorFlow models.
-    """
+    # declare distribution strategy
+    tf_dist_strategy = tf.distribute.MultiWorkerMirroredStrategy(communication_options=communication_options)
+
+    # get total number of workers
+    print("Number of devices: {}".format(tf_dist_strategy.num_replicas_in_sync))
+
+    return tf_dist_strategy, tf_dist_strategy.num_replicas_in_sync
