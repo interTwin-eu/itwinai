74d1d9c9a797d9d1b4966ab585f4076318250dfc
Matteo Bunino <matteo.bunino@gmail.com>
2024-04-12 17:42:34 +0200

    Refactor configurations and add documentation

--- tutorials/distributed-ml/torch-scaling-test/runall.sh
+++ tutorials/distributed-ml/torch-scaling-test/runall.sh
@@ -21,35 +21,35 @@ rm *.out *.err *.csv #*checkpoint.pth.tar
 # DDP baseline
 DIST_MODE="ddp"
 RUN_NAME="ddp-bl-imagenent"
-TRAINING_CMD="ddp_trainer.py -c config/base-config.yaml -c config/ddp-config.yaml"
+TRAINING_CMD="ddp_trainer.py -c config/base.yaml -c config/ddp.yaml"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
 
 # DeepSpeed baseline
 DIST_MODE="deepspeed"
 RUN_NAME="deepspeed-bl-imagenent"
-TRAINING_CMD="deepspeed_trainer.py -c config/base-config.yaml -c config/deepspeed-config.yaml"
+TRAINING_CMD="deepspeed_trainer.py -c config/base.yaml -c config/deepspeed.yaml"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
 
 # Horovod baseline
 DIST_MODE="horovod"
 RUN_NAME="horovod-bl-imagenent"
-TRAINING_CMD="horovod_trainer.py -c config/base-config.yaml -c config/horovod-config.yaml"
+TRAINING_CMD="horovod_trainer.py -c config/base.yaml -c config/horovod.yaml"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
 
 # DDP itwinai
 DIST_MODE="ddp"
 RUN_NAME="ddp-itwinai-imagenent"
-TRAINING_CMD="itwinai_trainer.py -c config/base-config.yaml -c config/ddp-config.yaml -s ddp"
+TRAINING_CMD="itwinai_trainer.py -c config/base.yaml -c config/ddp.yaml -s ddp"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
 
 # DeepSpeed itwinai
 DIST_MODE="deepspeed"
 RUN_NAME="deepspeed-itwinai-imagenent"
-TRAINING_CMD="itwinai_trainer.py -c config/base-config.yaml -c config/deepspeed-config.yaml -s deepspeed"
+TRAINING_CMD="itwinai_trainer.py -c config/base.yaml -c config/deepspeed.yaml -s deepspeed"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
 
 # Horovod itwinai
 DIST_MODE="horovod"
 RUN_NAME="horovod-itwinai-imagenent"
-TRAINING_CMD="itwinai_trainer.py -c config/base-config.yaml -c config/horovod-config.yaml -s horovod"
+TRAINING_CMD="itwinai_trainer.py -c config/base.yaml -c config/horovod.yaml -s horovod"
 sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD" --job-name="$RUN_NAME-n$N" --nodes=$N --time=$T slurm.sh
