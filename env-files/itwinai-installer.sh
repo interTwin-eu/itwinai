33de871dd1de220deff55838e433a532f7b06877
Matteo Bunino <matteo.bunino@gmail.com>
2024-06-01 10:25:03 +0200

    Cleanup

--- env-files/itwinai-installer.sh
+++ env-files/itwinai-installer.sh
@@ -3,10 +3,6 @@
 # Simple installation for users
 # It expects ML_FRAMEWORK env variable to be set
 
-# echo "Please choose an option:"
-# echo "1. Install itwinai with PyTorch support"
-# echo "2. Install itwinai with TensorFlow support"
-# read -p "Enter your choice (1 or 2): " choice
 
 if [ -z "$ML_FRAMEWORK" ]; then
     echo "Error: ML_FRAMEWORK env variable not set. Accepted values are 'pytorch' and 'tensorflow'"
