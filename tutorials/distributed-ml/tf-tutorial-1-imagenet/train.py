2200991b9e83c183d527c18560545d3f610b46d5
r-sarma <126173968+r-sarma@users.noreply.github.com>
2024-07-08 10:24:44 +0200

    Update train.py

--- tutorials/distributed-ml/tf-tutorial-1-imagenet/train.py
+++ tutorials/distributed-ml/tf-tutorial-1-imagenet/train.py
@@ -78,7 +78,7 @@ def tf_records_loader(files_path, shuffle=False):
     Returns:
         tf.data.Dataset: Returns dataset to be trained
     """
-    datasets = tf.data.Dataset.from_tensor_slices(files_path)
+    datasets = tf.data.Dataset.from_tensor_slices(files_path).repeat()
     datasets = datasets.shuffle(len(files_path)) if shuffle else datasets
     datasets = datasets.flat_map(tf.data.TFRecordDataset)
     datasets = datasets.map(
@@ -117,51 +117,51 @@ def main():
                       metrics=['accuracy']
                       )
 
-    # scale batch size with number of workers
-    batch_size = args.batch_size * get_strategy()[1]
+        # scale batch size with number of workers
+        batch_size = args.batch_size * get_strategy()[1]
 
-    dir_imagenet = args.data_dir+'imagenet-1K-tfrecords'
-    train_shard_suffix = 'train-*-of-01024'
-    test_shard_suffix = 'validation-*-of-00128'
-
-    train_set_path = sorted(
-        tf.io.gfile.glob(dir_imagenet + f'/{train_shard_suffix}')
-    )
-    test_set_path = sorted(
-        tf.io.gfile.glob(dir_imagenet + f'/{test_shard_suffix}')
-    )
-
-    train_dataset = tf_records_loader(train_set_path, shuffle=True)
-    test_dataset = tf_records_loader(test_set_path)
-
-    train_dataset = train_dataset.batch(
-        batch_size).prefetch(tf.data.experimental.AUTOTUNE)
-    test_dataset = test_dataset.batch(
-        batch_size).prefetch(tf.data.experimental.AUTOTUNE)
-
-    # distribute datasets among mirrored replicas
-    dist_train = strategy.experimental_distribute_dataset(
-        train_dataset
-    )
-    dist_test = strategy.experimental_distribute_dataset(
-        test_dataset
-    )
-
-    # TODO: add callbacks to evaluate per epoch time
-    et = timer()
-
-    # trains the model
-    model.fit(dist_train, epochs=args.epochs, steps_per_epoch=500, verbose=10)
-
-    print('TIMER: total epoch time:',
-          timer() - et, ' s')
-    print('TIMER: average epoch time:',
-          (timer() - et) / (args.epochs), ' s')
-
-    test_scores = model.evaluate(dist_test, steps=100, verbose=5)
-
-    print('Test loss:', test_scores[0])
-    print('Test accuracy:', test_scores[1])
+        dir_imagenet = args.data_dir+'imagenet-1K-tfrecords'
+        train_shard_suffix = 'train-*-of-01024'
+        test_shard_suffix = 'validation-*-of-00128'
+    
+        train_set_path = sorted(
+            tf.io.gfile.glob(dir_imagenet + f'/{train_shard_suffix}')
+        )
+        test_set_path = sorted(
+            tf.io.gfile.glob(dir_imagenet + f'/{test_shard_suffix}')
+        )
+    
+        train_dataset = tf_records_loader(train_set_path, shuffle=True)
+        test_dataset = tf_records_loader(test_set_path)
+    
+        train_dataset = train_dataset.batch(
+            batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+        test_dataset = test_dataset.batch(
+            batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    
+        # distribute datasets among mirrored replicas
+        dist_train = strategy.experimental_distribute_dataset(
+            train_dataset
+        )
+        dist_test = strategy.experimental_distribute_dataset(
+            test_dataset
+        )
+    
+        # TODO: add callbacks to evaluate per epoch time
+        et = timer()
+    
+        # trains the model
+        model.fit(dist_train, epochs=args.epochs, steps_per_epoch=500, verbose=10)
+    
+        print('TIMER: total epoch time:',
+              timer() - et, ' s')
+        print('TIMER: average epoch time:',
+              (timer() - et) / (args.epochs), ' s')
+    
+        test_scores = model.evaluate(dist_test, steps=100, verbose=5)
+    
+        print('Test loss:', test_scores[0])
+        print('Test accuracy:', test_scores[1])
 
 
 if __name__ == "__main__":
