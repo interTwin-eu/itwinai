ef8675dc7e2f8b8d058193d777ff4147105aea64
r-sarma <126173968+r-sarma@users.noreply.github.com>
2024-07-08 13:55:27 +0200

    Update train.py

--- tutorials/distributed-ml/tf-tutorial-0-basics/train.py
+++ tutorials/distributed-ml/tf-tutorial-0-basics/train.py
@@ -24,6 +24,10 @@ def parse_args() -> argparse.Namespace:
         default=64
     )
     parser.add_argument(
+        "--data_dir", type=str,
+        default='/p/scratch/intertwin/datasets/.keras/datasets/mnist.npz'
+    )
+    parser.add_argument(
         "--shuffle_dataloader",
         action=argparse.BooleanOptionalAction
     )
@@ -31,31 +35,30 @@ def parse_args() -> argparse.Namespace:
     args = parser.parse_args()
     return args
 
-
-def tf_rnd_dataset(args):
-    """Dummy TF dataset."""
-    (x_train, y_train), (x_test, y_test) = \
-        tf.keras.datasets.mnist.load_data(
-            path='/p/scratch/intertwin/datasets/.keras/datasets/mnist.npz')
-
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).repeat()
-    train_dataset = train_dataset.batch(args.batch_size)
-
-    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).repeat()
-    test_dataset = test_dataset.batch(args.batch_size)
-
-    return train_dataset, test_dataset
-
-
 def trainer_entrypoint_fn(
-        foo: Any, args: argparse.Namespace, strategy
+        foo: Any, 
+        args: argparse.Namespace, 
+        strategy,
+        num_replicas
 ) -> int:
-    """Dummy training function, similar to custom code developed
+    """Training function, similar to custom code developed
     by some use case.
     """
     # dataset to be trained
-    train_dataset, test_dataset = tf_rnd_dataset(args)
+    (x_train, y_train), (x_test, y_test) = \
+        tf.keras.datasets.mnist.load_data(
+            path=args.data_dir)
 
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+    # scale batch size with number of workers
+    batch_size = args.batch_size * num_replicas
+
+    # batching dataset and repeat
+    train_dataset = train_dataset.batch(batch_size).repeat()
+    test_dataset = test_dataset.batch(batch_size).repeat()
+    
     # distribute datasets among mirrored replicas
     dist_train = strategy.experimental_distribute_dataset(
         train_dataset
@@ -81,9 +84,11 @@ def trainer_entrypoint_fn(
 
         model.fit(dist_train,
                   epochs=5,
-                  steps_per_epoch=2000)
+                  steps_per_epoch=len(x_train)//batch_size)
 
-        test_scores = model.evaluate(dist_test, verbose=0, steps=500)
+        test_scores = model.evaluate(dist_test, 
+                                     verbose=1, 
+                                     steps=len(x_test)//batch_size)
 
         print('Test loss:', test_scores[0])
         print('Test accuracy:', test_scores[1])
@@ -105,4 +110,4 @@ if __name__ == "__main__":
             f"Strategy {args.strategy} is not recognized/implemented.")
 
     # Launch distributed training
-    trainer_entrypoint_fn("foobar", args, strategy)
+    trainer_entrypoint_fn("foobar", args, strategy, num_replicas)
