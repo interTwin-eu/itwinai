ab945748cc4638b5394a3c45a5d79e6ccbc82cc7
Matteo Bunino <matteo.bunino@gmail.com>
2024-07-24 14:45:28 +0200

    FIX errors

--- use-cases/3dgan/run-provenance-experiments.sh
+++ use-cases/3dgan/run-provenance-experiments.sh
@@ -6,7 +6,9 @@ mkdir slurm_logs
 source ../../.venv-pytorch/bin/activate
 itwinai exec-pipeline --config config.yaml --pipe-key training_pipeline 1> slurm_logs/1_worker.out 2> slurm_logs/1_worker.err
 
-sbatch --wait --nodes=1 --output=slurm_logs/4_worker.out --error=slurm_logs/4_worker.err slurm.vega.sh
-sbatch --wait --nodes=2 --output=slurm_logs/8_worker.out --error=slurm_logs/8_worker.err slurm.vega.sh
-# sbatch --wait --nodes=4 --output=slurm_logs/16_worker.out --error=slurm_logs/16_worker.err slurm.vega.sh
-# sbatch --wait --nodes=8 --output=slurm_logs/32_worker.out --error=slurm_logs/32_worker.err slurm.vega.sh
+SLURM_SCRIPT="slurm.vega.sh"
+
+sbatch --wait --nodes=1 --output=slurm_logs/4_worker.out --error=slurm_logs/4_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=2 --output=slurm_logs/8_worker.out --error=slurm_logs/8_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=4 --output=slurm_logs/16_worker.out --error=slurm_logs/16_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=8 --output=slurm_logs/32_worker.out --error=slurm_logs/32_worker.err $SLURM_SCRIPT
