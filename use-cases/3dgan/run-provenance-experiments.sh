1c236febfbb506559a2f2a71a5707211a4dfa4ca
Matteo Bunino <matteo.bunino@gmail.com>
2024-07-25 09:28:02 +0200

    UPDATE scripts

--- use-cases/3dgan/run-provenance-experiments.sh
+++ use-cases/3dgan/run-provenance-experiments.sh
@@ -8,17 +8,20 @@ mkdir slurm_logs
 # ml Stages/2024 GCC CUDA/12 cuDNN Python
 
 # SLURM_SCRIPT="slurm.jsc.sh"
+# source ../../envAI_hdfml/bin/activate
 # ========== FOR JSC ==========
 
 # ========== FOR Vega ==========
 SLURM_SCRIPT="slurm.vega.sh"
+source ../../.venv-pytorch/bin/activate
 # ========== FOR Vega ==========
 
-source ../../envAI_hdfml/bin/activate
+# Launch experiments
+
+# 1 worker: no SLURM needed
 itwinai exec-pipeline --config config.yaml --pipe-key training_pipeline 1> slurm_logs/1_worker.out 2> slurm_logs/1_worker.err
 
-
-
+# 4, 8, 16... workers
 sbatch --wait --nodes=1 --output=slurm_logs/4_worker.out --error=slurm_logs/4_worker.err $SLURM_SCRIPT
 sbatch --wait --nodes=2 --output=slurm_logs/8_worker.out --error=slurm_logs/8_worker.err $SLURM_SCRIPT
 sbatch --wait --nodes=4 --output=slurm_logs/16_worker.out --error=slurm_logs/16_worker.err $SLURM_SCRIPT
