106663def7dd59c67220e6f33bf0eed3389a1fc9
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-07-25 09:30:26 +0200

    Prov4ml integration (#192)
    * ADD prov4ml logger
    
    * UPDATE enum access fields
    
    * UPDATE loggers documentation and first integration attempt
    
    * ADD prov logger
    
    * format kinds table
    
    * MIGRATE to upstream prov4ml
    
    * ADD docs build on JSC
    
    * ADD RTD website
    
    * UPDATE docs creation
    
    * Refactor
    
    * UPDATE logger
    
    * Remove lightning callbacks and loggers
    
    * ADD checkpoints
    
    * UPDATE logger kind docs
    
    * Update README.md
    
    * ADD rank on loggers
    
    * Update loggers.py
    
    * Update loggers.py
    
    * Update loggers.py
    
    * Update loggers.py
    
    * Update loggers.py
    
    * FIX linter
    
    * REFACTOR loggers
    
    * Simplify prov4ml switch case
    
    * UPDATE loggers
    
    * FIX prov graph
    
    * REFACTOR itwinai logging
    
    * UPDATE SLURM jobscripts
    
    * REFACTOR
    
    * Update
    
    * ADD prov experiments
    
    * REFACTOR provenance logs and SLURM jobscripts
    
    * REMOVE duplication
    
    * FIX dataset name
    
    * UPDATE README
    
    * SKIP cyclones use case
    
    * UPDATE version
    
    * REMOVE redundant parameter
    
    * CLEANUP
    
    * ADD warning
    
    * ADD warning
    
    * UPDATE README
    
    * FIX errors
    
    * ADD docs
    
    * UPDATE scripts
    
    * UPDATE scripts

--- use-cases/3dgan/run-provenance-experiments.sh
+++ use-cases/3dgan/run-provenance-experiments.sh
@@ -3,10 +3,26 @@
 rm -rf slurm_logs mllogs
 mkdir slurm_logs
 
+# ========== FOR JSC ==========
+# ml --force purge
+# ml Stages/2024 GCC CUDA/12 cuDNN Python
+
+# SLURM_SCRIPT="slurm.jsc.sh"
+# source ../../envAI_hdfml/bin/activate
+# ========== FOR JSC ==========
+
+# ========== FOR Vega ==========
+SLURM_SCRIPT="slurm.vega.sh"
 source ../../.venv-pytorch/bin/activate
+# ========== FOR Vega ==========
+
+# Launch experiments
+
+# 1 worker: no SLURM needed
 itwinai exec-pipeline --config config.yaml --pipe-key training_pipeline 1> slurm_logs/1_worker.out 2> slurm_logs/1_worker.err
 
-sbatch --wait --nodes=1 --output=slurm_logs/4_worker.out --error=slurm_logs/4_worker.err slurm.vega.sh
-sbatch --wait --nodes=2 --output=slurm_logs/8_worker.out --error=slurm_logs/8_worker.err slurm.vega.sh
-# sbatch --wait --nodes=4 --output=slurm_logs/16_worker.out --error=slurm_logs/16_worker.err slurm.vega.sh
-# sbatch --wait --nodes=8 --output=slurm_logs/32_worker.out --error=slurm_logs/32_worker.err slurm.vega.sh
+# 4, 8, 16... workers
+sbatch --wait --nodes=1 --output=slurm_logs/4_worker.out --error=slurm_logs/4_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=2 --output=slurm_logs/8_worker.out --error=slurm_logs/8_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=4 --output=slurm_logs/16_worker.out --error=slurm_logs/16_worker.err $SLURM_SCRIPT
+sbatch --wait --nodes=8 --output=slurm_logs/32_worker.out --error=slurm_logs/32_worker.err $SLURM_SCRIPT
