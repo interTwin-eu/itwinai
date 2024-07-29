505e1f066e19fc558497ef68678b85b92b3b37b1
r-sarma <r.sarma@fz-juelich.de>
2024-07-29 15:27:40 +0200

    seasons launch iteratively and fixes

--- use-cases/xtclim/src/trainer.py
+++ use-cases/xtclim/src/trainer.py
@@ -48,6 +48,7 @@ class XTClimTrainer(TorchTrainer):
             epochs: int,
             batch_size: int,
             lr: float,
+            seasons: Literal["winter_", "spring_", "summer_", "autumn_"] = 'winter_',
             strategy: Literal["ddp", "deepspeed", "horovod"] = 'ddp',
             save_best: bool = True,
             logger: Optional[Logger] = None
@@ -58,13 +59,18 @@ class XTClimTrainer(TorchTrainer):
             strategy=strategy,
             logger=logger
         )
-        self.batch_size = batch_size
         self.epochs = epochs
-        self.lr = lr
+        self.seasons = seasons
         # Global training configuration
         self.config = TrainingConfiguration(
-            batch_size=self.batch_size,
+            batch_size = batch_size,
+            lr = lr,
             save_best=save_best,
+            n_memb = 1, # number of members used in training the network
+            stop_delta = 0.01,  # under 1% improvement consider the model starts converging
+            patience = 15,  # wait for a few epochs to be sure before actually stopping
+            early_count = 0,  # count when validation loss < stop_delta
+            old_valid_loss = 0  # keep track of validation loss at t-1
         )
 
     def final_loss(self, bce_loss, mu, logvar, beta=0.1):
@@ -72,195 +78,187 @@ class XTClimTrainer(TorchTrainer):
         Adds up reconstruction loss (BCELoss) and Kullback-Leibler divergence.
         KL-Divergence = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
 
-        Parameters:
-        bce_loss: recontruction loss
-        mu: the mean from the latent vector
-        logvar: log variance from the latent vector
-        beta: weight over the KL-Divergence
+        Args:
+            - bce_loss (torch.Tensor): recontruction loss
+            - mu (torch.Tensor): the mean from the latent vector
+            - logvar (torch.Tensor): log variance from the latent vector
+            - beta (torch.Tensor): weight over the KL-Divergence
+
+        Returns:
+            - total loss (torch.Tensor)
         """
         BCE = bce_loss
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return BCE + beta*KLD
 
-
     @monitor_exec
     def execute(self):
 
         # pick the season to study among:
-        # '' (none, i.e. full dataset), 'winter_', 'spring_', 'summer_', 'autumn_'
-        seasons = ["winter_", "spring_", "summer_", "autumn_"]
-
-        # number of members used for the training of the network
-        n_memb = 1
-
-        #early stopping parameters
-        stop_delta = 0.01  # under 1% improvement consider the model starts converging
-        patience = 15  # wait for a few epochs to be sure before actually stopping
-        early_count = 0  # count when validation loss < stop_delta
-        old_valid_loss = 0  # keep track of validation loss at t-1
+        season = self.seasons
 
         # Initialize distributed backend
         self._init_distributed_strategy()
-        for season in seasons:
-            # initialize the model
-            cvae_model = model.ConvVAE()
-            optimizer = optim.Adam(cvae_model.parameters(), lr=self.lr)
+        # initialize the model
+        cvae_model = model.ConvVAE()
+        optimizer = optim.Adam(cvae_model.parameters(), lr=self.config.lr)
 
-            # First, define strategy-wise optional configurations
-            if isinstance(self.strategy, DeepSpeedStrategy):
-                # Batch size definition is not optional for DeepSpeedStrategy!
-                distribute_kwargs = dict(
-                    config_params=dict(
-                        train_micro_batch_size_per_gpu=self.config.batch_size
+        # First, define strategy-wise optional configurations
+        if isinstance(self.strategy, DeepSpeedStrategy):
+            # Batch size definition is not optional for DeepSpeedStrategy!
+            distribute_kwargs = dict(
+                config_params=dict(
+                    train_micro_batch_size_per_gpu=self.config.batch_size
+                )
+            )
+        else:
+            distribute_kwargs = {}
+
+        # Distributed model, optimizer, and scheduler
+        cvae_model, optimizer, _ = self.strategy.distributed(
+            cvae_model, optimizer, **distribute_kwargs
+        )
+
+        # load training set and train data
+        train_time = pd.read_csv(f"input/dates_train_{season}data_{self.config.n_memb}memb.csv")
+        train_data = np.load(
+            f"input/preprocessed_1d_train_{season}data_{self.config.n_memb}memb.npy"
+        )
+        n_train = len(train_data)
+        trainset = [
+            (torch.from_numpy(np.reshape(train_data[i], (2, 32, 32))), train_time["0"][i])
+            for i in range(n_train)
+        ]
+        # load train set, shuffle it, and create batches
+        trainloader = self.strategy.create_dataloader(trainset, batch_size=self.config.batch_size,
+                shuffle=True, pin_memory=True)
+
+        # load validation set and validation data
+        test_time = pd.read_csv(f"input/dates_test_{season}data_{self.config.n_memb}memb.csv")
+        test_data = np.load(f"input/preprocessed_1d_test_{season}data_{self.config.n_memb}memb.npy")
+        n_test = len(test_data)
+        testset = [
+            (torch.from_numpy(np.reshape(test_data[i], (2, 32, 32))), test_time["0"][i])
+            for i in range(n_test)
+        ]
+        testloader = self.strategy.create_dataloader(testset, batch_size=self.config.batch_size,
+                shuffle=False, pin_memory=True)
+
+        if self.strategy.is_main_worker and self.logger:
+            self.logger.create_logger_context()
+
+        # a list to save all the reconstructed images in PyTorch grid format
+        grid_images = []
+        # a list to save the loss evolutions
+        train_loss = []
+        valid_loss = []
+        min_valid_epoch_loss = float('inf')  # random high value
+
+        for epoch in range(self.epochs):
+            if self.strategy.is_main_worker:
+                print(f"Epoch {epoch+1} of {self.epochs}")
+
+            if self.strategy.is_distributed:
+                # Inform the sampler that a new epoch started: shuffle
+                # may be needed
+                trainloader.sampler.set_epoch(epoch)
+                testloader.sampler.set_epoch(epoch)
+
+            # train the model
+            train_epoch_loss = train(
+                cvae_model, trainloader, trainset, self.device, optimizer, criterion, beta
+            )
+
+            # evaluate the model on the test set
+            valid_epoch_loss, recon_images = validate(
+                cvae_model, testloader, testset, self.device, criterion, beta
+            )
+
+            self.log(train_epoch_loss,
+                    'epoch_train_loss',
+                    kind='metric'
                     )
-                )
+            self.log(valid_epoch_loss,
+                    'epoch_valid_loss',
+                    kind='metric'
+                    )
+
+            # keep track of the losses
+            train_loss.append(train_epoch_loss)
+            valid_loss.append(valid_epoch_loss)
+
+            # save the reconstructed images from the validation loop
+            #save_reconstructed_images(recon_images, epoch+1, season)
+
+            # convert the reconstructed images to PyTorch image grid format
+            image_grid = make_grid(recon_images.detach().cpu())
+            grid_images.append(image_grid)
+            # save one example of reconstructed image before and after training
+
+            #if epoch == 0 or epoch == self.epochs-1:
+            #    save_ex(recon_images[0], epoch, season)
+
+            # decreasing learning rate
+            if (epoch + 1) % 20 == 0:
+                self.config.lr = self.config.lr / 5
+
+            # early stopping to avoid overfitting
+            if (
+                epoch > 1
+                and (self.config.old_valid_loss - valid_epoch_loss) / self.config.old_valid_loss < self.config.stop_delta
+            ):
+                # if the marginal improvement in validation loss is too small
+                self.config.early_count += 1
+                if self.config.early_count > self.config.patience:
+                # if too small improvement for a few epochs in a row, stop learning
+                    save_ex(recon_images[0], epoch, season)
+                    break
+
             else:
-                distribute_kwargs = {}
+                # if the condition is not verified anymore, reset the count
+                self.config.early_count = 0
+            self.config.old_valid_loss = valid_epoch_loss
 
-            # Distributed model, optimizer, and scheduler
-            cvae_model, optimizer, _ = self.strategy.distributed(
-                cvae_model, optimizer, **distribute_kwargs
-            )
+            # save best model
+            worker_val_losses = self.strategy.gather_obj(valid_epoch_loss)
+            if self.strategy.is_main_worker:
+                # Save only in the main worker
+                # avg_loss has a meaning only in the main worker
+                avg_loss = np.mean(worker_val_losses)
+                if self.config.save_best and avg_loss < min_valid_epoch_loss:
+                    min_valid_epoch_loss = avg_loss
+                    checkpoint = {
+                        'epoch': epoch,
+                        'model_state_dict': cvae_model.state_dict(),
+                        'optim_state_dict': optimizer.state_dict(),
+                        'val_loss': valid_epoch_loss,
+                    }
+                    # save checkpoint only if it is better than
+                    # the previous ones
+                    checkpoint_filename = f"outputs/cvae_model_{season}1d_{self.config.n_memb}memb.pth"
+                    torch.save(checkpoint, checkpoint_filename)
+                    # itwinai - log checkpoint as artifact
+                    self.log(checkpoint_filename,
+                            os.path.basename(checkpoint_filename),
+                            kind='artifact')
 
-            # load training set and train data
-            train_time = pd.read_csv(f"input/dates_train_{season}data_{n_memb}memb.csv")
-            train_data = np.load(
-                f"input/preprocessed_1d_train_{season}data_{n_memb}memb.npy"
-            )
-            n_train = len(train_data)
-            trainset = [
-                (torch.from_numpy(np.reshape(train_data[i], (2, 32, 32))), train_time["0"][i])
-                for i in range(n_train)
-            ]
-            # load train set, shuffle it, and create batches
-            trainloader = self.strategy.create_dataloader(trainset, batch_size=self.batch_size,
-                    shuffle=True, pin_memory=True)
+        print(f"Train Loss: {train_epoch_loss:.4f}")
+        print(f"Val Loss: {valid_epoch_loss:.4f}")
 
-            # load validation set and validation data
-            test_time = pd.read_csv(f"input/dates_test_{season}data_{n_memb}memb.csv")
-            test_data = np.load(f"input/preprocessed_1d_test_{season}data_{n_memb}memb.npy")
-            n_test = len(test_data)
-            testset = [
-                (torch.from_numpy(np.reshape(test_data[i], (2, 32, 32))), test_time["0"][i])
-                for i in range(n_test)
-            ]
-            testloader = self.strategy.create_dataloader(testset, batch_size=self.batch_size,
-                    shuffle=False, pin_memory=True)
+        save_loss_plot(train_loss, valid_loss, season)
+        # save the loss evolutions
+        pd.DataFrame(train_loss).to_csv(
+            f"outputs/train_loss_indiv_{season}1d_{self.config.n_memb}memb.csv"
+        )
+        pd.DataFrame(valid_loss).to_csv(
+            f"outputs/test_loss_indiv_{season}1d_{self.config.n_memb}memb.csv"
+        )
 
-            if self.strategy.is_main_worker and self.logger:
-                self.logger.create_logger_context()
+        # Clean-up
+        if self.strategy.is_main_worker and self.logger:
+            self.logger.destroy_logger_context()
 
-            # a list to save all the reconstructed images in PyTorch grid format
-            grid_images = []
-            # a list to save the loss evolutions
-            train_loss = []
-            valid_loss = []
-            min_valid_epoch_loss = float('inf')  # random high value
-
-            for epoch in range(self.epochs):
-                if self.strategy.is_main_worker:
-                    print(f"Epoch {epoch+1} of {self.epochs}")
-
-                if self.strategy.is_distributed:
-                    # Inform the sampler that a new epoch started: shuffle
-                    # may be needed
-                    trainloader.sampler.set_epoch(epoch)
-                    testloader.sampler.set_epoch(epoch)
-
-                # train the model
-                train_epoch_loss = train(
-                    cvae_model, trainloader, trainset, self.device, optimizer, criterion, beta
-                )
-
-                # evaluate the model on the test set
-                valid_epoch_loss, recon_images = validate(
-                    cvae_model, testloader, testset, self.device, criterion, beta
-                )
-
-                self.log(train_epoch_loss,
-                         'epoch_train_loss',
-                         kind='metric'
-                         )
-                self.log(valid_epoch_loss,
-                          'epoch_valid_loss',
-                          kind='metric'
-                          )
-
-                # keep track of the losses
-                train_loss.append(train_epoch_loss)
-                valid_loss.append(valid_epoch_loss)
-
-                # save the reconstructed images from the validation loop
-                #save_reconstructed_images(recon_images, epoch+1, season)
-
-                # convert the reconstructed images to PyTorch image grid format
-                image_grid = make_grid(recon_images.detach().cpu())
-                grid_images.append(image_grid)
-                # save one example of reconstructed image before and after training
-
-                #if epoch == 0 or epoch == self.epochs-1:
-                #    save_ex(recon_images[0], epoch, season)
-
-                # decreasing learning rate
-                if (epoch + 1) % 20 == 0:
-                    lr = lr / 5
-
-                # early stopping to avoid overfitting
-                if (
-                    epoch > 1
-                    and (old_valid_loss - valid_epoch_loss) / old_valid_loss < stop_delta
-                ):
-                    # if the marginal improvement in validation loss is too small
-                    early_count += 1
-                    if early_count > patience:
-                    # if too small improvement for a few epochs in a row, stop learning
-                        save_ex(recon_images[0], epoch, season)
-                        break
-
-                else:
-                    # if the condition is not verified anymore, reset the count
-                    early_count = 0
-                old_valid_loss = valid_epoch_loss
-
-                # save best model
-                worker_val_losses = self.strategy.gather_obj(valid_epoch_loss)
-                if self.strategy.is_main_worker:
-                    # Save only in the main worker
-                    # avg_loss has a meaning only in the main worker
-                    avg_loss = np.mean(worker_val_losses)
-                    if self.config.save_best and avg_loss < min_valid_epoch_loss:
-                        min_valid_epoch_loss = avg_loss
-                        checkpoint = {
-                            'epoch': epoch,
-                            'model_state_dict': cvae_model.state_dict(),
-                            'optim_state_dict': optimizer.state_dict(),
-                            'val_loss': valid_epoch_loss,
-                        }
-                        # save checkpoint only if it is better than
-                        # the previous ones
-                        checkpoint_filename = f"outputs/cvae_model_{season}1d_{n_memb}memb.pth"
-                        torch.save(checkpoint, checkpoint_filename)
-                        # itwinai - log checkpoint as artifact
-                        self.log(checkpoint_filename,
-                                 os.path.basename(checkpoint_filename),
-                                 kind='artifact')
-
-            print(f"Train Loss: {train_epoch_loss:.4f}")
-            print(f"Val Loss: {valid_epoch_loss:.4f}")
-
-            save_loss_plot(train_loss, valid_loss, season)
-            # save the loss evolutions
-            pd.DataFrame(train_loss).to_csv(
-                f"outputs/train_loss_indiv_{season}1d_{n_memb}memb.csv"
-            )
-            pd.DataFrame(valid_loss).to_csv(
-                f"outputs/test_loss_indiv_{season}1d_{n_memb}memb.csv"
-            )
-
-            # Clean-up
-            if self.strategy.is_main_worker and self.logger:
-                self.logger.destroy_logger_context()
         self.strategy.clean_up()
 
-        # emissions = tracker.stop()
-        # print(f"Emissions from this training run: {emissions:.5f} kg CO2eq")
+    # emissions = tracker.stop()
+    # print(f"Emissions from this training run: {emissions:.5f} kg CO2eq")
--- use-cases/xtclim/train.py
+++ use-cases/xtclim/train.py
@@ -7,24 +7,30 @@ from typing import Dict
 import argparse
 import logging
 from datetime import datetime
+import yaml
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'preprocessing'))
 
-from itwinai.parser import ConfigParser, ArgumentParser
+from itwinai.parser import ConfigParser
+
+def read_config(file_path):
+    with open(file_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument(
-         "-p", "--pipeline", type=str, required=True,
-         help='Configuration file to the pipeline to execute.'
-    )
-    args = parser.parse_args()
-
-    pipe_parser = ConfigParser(
-         config=args.pipeline,
-    )
-
-    pipeline = pipe_parser.parse_pipeline()
-    pipeline.execute()
+    # read the config file defined in pipeline.yaml
+    config = read_config('pipeline.yaml')
+    # load the list of seasons
+    seasons_list = config['seasons']
+    # loop over the seasons and launch pipelines iteratively
+    for season in seasons_list:
+        config['pipeline']['init_args']['steps']['training-step']['init_args']['seasons'] = season
+        pipe_parser = ConfigParser(
+            config=config,
+        )
+        pipeline = pipe_parser.parse_pipeline()
+        print(f"Running pipeline for season: {season}")
+        pipeline.execute()
 
