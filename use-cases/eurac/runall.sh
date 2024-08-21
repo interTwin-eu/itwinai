828624f312de1471115d944ca1e80af7d7b2b44b
MutegekiHenry <henrymutegeki11@gmail.com>
2024-08-21 18:22:35 +0200

    add scaling tests

---
+++ use-cases/eurac/runall.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Run all versions of distributed ML version
+# $1 (Optional[int]): number of nodes. Default: 2
+# $2 (Optional[str]): timeout. Default: "00:30:00"
+
+if [ -z "$1" ] ; then
+    N=2
+else
+    N=$1
+fi
+if [ -z "$2" ] ; then
+    T="00:30:00"
+else
+    T=$2
+fi
+# Python virtual environment (no conda/micromamba)
+PYTHON_VENV="../../../envAI_hdfml"
+CMD="--nodes=$N --time=$T --account=intertwin --partition=batch slurm.sh"
+
+echo "Distributing training over $N nodes. Timeout set to: $T"
+
+# Clear SLURM logs (*.out and *.err files)
+rm -rf logs_slurm checkpoints*
+mkdir logs_slurm
+rm -rf logs_torchrun
+# Clear scaling test logs 
+rm *.csv
+
+# DDP itwinai
+DIST_MODE="ddp"
+RUN_NAME="ddp-itwinai"
+TRAINING_CMD="torch_dist_final_scaling.py --strategy ddp"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    $CMD
+
+# DeepSpeed itwinai
+DIST_MODE="deepspeed"
+RUN_NAME="deepspeed-itwinai"
+TRAINING_CMD="torch_dist_final_scaling.py --strategy deepspeed"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    $CMD
+
+# Horovod itwinai
+DIST_MODE="horovod"
+RUN_NAME="horovod-itwinai"
+TRAINING_CMD="torch_dist_final_scaling.py -s horovod"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    $CMD
