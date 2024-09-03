18731a75bf25ce14bfb38d123e1fea9bba40db88
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-09-03 10:35:24 +0200

    Update run_docker.sh

--- tutorials/distributed-ml/torch-tutorial-containers/run_docker.sh
+++ tutorials/distributed-ml/torch-tutorial-containers/run_docker.sh
@@ -7,12 +7,12 @@ if [ -z "$1" ]; then
     # CPU only execution
     docker run -it --rm --name mnist-training --user $UID:$GID \
         --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-        -v "$PWD":/use-case  ghcr.io/intertwin-eu/itwinai:0.0.1-torch-2.1 \
+        -v "$PWD":/use-case  ghcr.io/intertwin-eu/itwinai:0.2.2-torch-2.1 \
         /bin/bash -c "cd /use-case && $CMD"
 elif [ "$1" == "gpu" ]; then
     # With GPU support: --gpus all
     docker run -it --rm --name mnist-training --user $UID:$GID \
         --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-        -v "$PWD":/use-case  ghcr.io/intertwin-eu/itwinai:0.0.1-torch-2.1 \
+        -v "$PWD":/use-case  ghcr.io/intertwin-eu/itwinai:0.2.2-torch-2.1 \
         /bin/bash -c "cd /use-case && $CMD"
 fi
