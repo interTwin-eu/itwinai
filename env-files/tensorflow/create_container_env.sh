55a66dc5e6c6e98d9313fc35108af6c2f356f2b0
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-09-04 17:23:59 +0200

    Update create_container_env.sh

--- env-files/tensorflow/create_container_env.sh
+++ env-files/tensorflow/create_container_env.sh
@@ -10,7 +10,14 @@ pip install --no-cache-dir --upgrade pip
 # Since TF 2.16, keras updated to 3.3,
 # which leads to an error when more than 1 node is used
 # https://keras.io/getting_started/
-pip install --no-cache-dir tf_keras==2.16.*
+pip install --no-cache-dir tf_keras==2.16.* || exit 1
+
+# Install Pov4ML
+if [[ "$OSTYPE" =~ ^darwin ]] ; then
+  pip install "prov4ml[apple]@git+https://github.com/matbun/ProvML@main"
+else
+  pip install "prov4ml[linux]@git+https://github.com/matbun/ProvML@main"
+fi
 
 # itwinai
 pip --no-cache-dir install .
