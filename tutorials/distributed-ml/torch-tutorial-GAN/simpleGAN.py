cc28b438e297295bd356862a59763bb6095a4b54
Jarl Sondre Sæther <jarl_sondre@hotmail.com>
2024-09-13 09:20:36 +0200

    update small stuff in tutorials

--- tutorials/distributed-ml/torch-tutorial-GAN/simpleGAN.py
+++ tutorials/distributed-ml/torch-tutorial-GAN/simpleGAN.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -8,9 +9,7 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as dset
 import torchvision.utils as vutils
-import matplotlib.pyplot as plt
 
-CUDA = False
 DATA_PATH = '../data'
 BATCH_SIZE = 128
 IMAGE_CHANNEL = 1
@@ -24,14 +23,15 @@ FAKE_LABEL = 0
 lr = 2e-4
 seed = 1
 
-CUDA = CUDA and torch.cuda.is_available()
+USE_CUDA = False
+CUDA = torch.cuda.is_available() and USE_CUDA
+device = torch.device("cuda:0" if CUDA else "cpu")
+
 print("PyTorch version: {}".format(torch.__version__))
 if CUDA:
     print("CUDA version: {}\n".format(torch.version.cuda))
-
-if CUDA:
     torch.cuda.manual_seed(seed)
-device = torch.device("cuda:0" if CUDA else "cpu")
+
 cudnn.benchmark = True
 
 # Data preprocessing
