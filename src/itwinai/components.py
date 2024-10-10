d35c2a7b82b9e4182373621220405e0e3e5383e5
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-10-10 14:05:44 +0200

    Split decorator into two and add tests

--- src/itwinai/components.py
+++ src/itwinai/components.py
@@ -85,60 +85,76 @@ Example:
 
 from __future__ import annotations
 
-import builtins as __builtin__
 import functools
 import time
 from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
-from .distributed import (
-    builtin_print,
-    detect_distributed_environment,
-    distributed_patch_print,
-)
 from .serialization import ModelLoader, Serializable
 from .type import MLArtifact, MLDataset, MLModel
 
 
-def monitor_exec(print_on_main_worker_only: bool = False) -> Callable:
-    """Decorator for execute method of a component class.
-    Computes execution time and gives some information about
-    the execution of the component.
-
-    Args:
-        print_on_main_worker_only (bool): whether to ignore ``print()`` calls on
-            workers which are not the main worker in a distributed context,
-            such as distributed ML. If set to ``True``, to force printing on
-            all workers you will need to use ``print(..., force=True)``.
+def monitor_exec(method: Callable) -> Callable:
+    """
+    Decorator for ``BaseComponent``'s methods.
+    Prints when the component starts and ends executing, indicating
+    its execution time.
     """
 
-    def method_decorator(method: Callable) -> Callable:
-        @functools.wraps(method)
-        def wrapper(self: BaseComponent, *args, **kwargs) -> Any:
-            if print_on_main_worker_only:
-                # Disable print in workers different from the main one,
-                # when in distributed environments.
-                dist_grank = detect_distributed_environment().global_rank
-                distributed_patch_print(is_main=dist_grank == 0)
+    @functools.wraps(method)
+    def wrapper(self: BaseComponent, *args, **kwargs) -> Any:
+        msg = f"Starting execution of '{self.name}'..."
+        self._printout(msg)
+        start_t = time.time()
+        try:
+            result = method(self, *args, **kwargs)
+        finally:
+            self.cleanup()
+        self.exec_t = time.time() - start_t
+        msg = f"'{self.name}' executed in {self.exec_t:.3f}s"
+        self._printout(msg)
+        return result
+    return wrapper
 
-            msg = f"Starting execution of '{self.name}'..."
-            self._printout(msg)
-            start_t = time.time()
-            try:
-                result = method(self, *args, **kwargs)
-            finally:
-                self.cleanup()
-            self.exec_t = time.time() - start_t
-            msg = f"'{self.name}' executed in {self.exec_t:.3f}s"
-            self._printout(msg)
+# def monitor_exec(print_on_main_worker_only: bool = False) -> Callable:
+#     """Decorator for execute method of a component class.
+#     Computes execution time and gives some information about
+#     the execution of the component.
 
-            # Reset print() to __builtins__ one
-            __builtin__.print = builtin_print
+#     Args:
+#         print_on_main_worker_only (bool): whether to ignore ``print()`` calls on
+#             workers which are not the main worker in a distributed context,
+#             such as distributed ML. If set to ``True``, to force printing on
+#             all workers you will need to use ``print(..., force=True)``.
+#     """
 
-            return result
+#     def method_decorator(method: Callable) -> Callable:
+#         @functools.wraps(method)
+#         def wrapper(self: BaseComponent, *args, **kwargs) -> Any:
+#             if print_on_main_worker_only:
+#                 # Disable print in workers different from the main one,
+#                 # when in distributed environments.
+#                 dist_grank = detect_distributed_environment().global_rank
+#                 patched_print = distributed_patch_print(is_main=dist_grank == 0)
+#                 __builtin__.print = patched_print
 
-        return wrapper
-    return method_decorator
+#             msg = f"Starting execution of '{self.name}'..."
+#             self._printout(msg)
+#             start_t = time.time()
+#             try:
+#                 result = method(self, *args, **kwargs)
+#             finally:
+#                 self.cleanup()
+#             self.exec_t = time.time() - start_t
+#             msg = f"'{self.name}' executed in {self.exec_t:.3f}s"
+#             self._printout(msg)
+
+#             # Reset print() to __builtins__ one
+#             __builtin__.print = builtin_print
+
+#             return result
+#         return wrapper
+#     return method_decorator
 
 
 class BaseComponent(ABC, Serializable):
