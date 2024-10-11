4855371f0849d9a90a0423999296b8fc127c9946
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-10-11 18:22:24 +0200

    ADD basic torch container build

---
+++ ci/src/main/__init__.py
@@ -0,0 +1,65 @@
+"""A generated module for Itwinai functions
+
+This module has been generated via dagger init and serves as a reference to
+basic module structure as you get started with Dagger.
+
+Two functions have been pre-created. You can modify, delete, or add to them,
+as needed. They demonstrate usage of arguments and return types using simple
+echo and grep commands. The functions can be called from the dagger CLI or
+from one of the SDKs.
+
+The first line in this comment block is a short description line and the
+rest is a long description with more detail on the module's purpose or usage,
+if appropriate. All modules should have a short description.
+"""
+
+# NOTE: it's recommended to move your code into other files in this package
+# and keep __init__.py for imports only, according to Python's convention.
+# The only requirement is that Dagger needs to be able to import a package
+# called "main" (i.e., src/main/).
+#
+# For example, to import from src/main/main.py:
+# >>> from .main import Itwinai as Itwinai
+
+from typing import Annotated
+
+import dagger
+from dagger import dag, function, object_type, Doc
+
+
+@object_type
+class Itwinai:
+
+    @function
+    def build_torch(
+        self,
+        src: Annotated[
+            dagger.Directory,
+            Doc("location of directory containing Dockerfile"),
+        ],
+    ) -> dagger.Container:
+        """Build image from existing Dockerfile"""
+        return (
+            dag.container()
+            .with_directory("/src", src)
+            .with_workdir("/src")
+            .directory("/src")
+            .docker_build()
+        )
+
+    @function
+    def container_echo(self, string_arg: str) -> dagger.Container:
+        """Returns a container that echoes whatever string argument is provided"""
+        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+
+    @function
+    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
+        """Returns lines that match a pattern in the files of the provided Directory"""
+        return await (
+            dag.container()
+            .from_("alpine:latest")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt")
+            .with_exec(["grep", "-R", pattern, "."])
+            .stdout()
+        )
