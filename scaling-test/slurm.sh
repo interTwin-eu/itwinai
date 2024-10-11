2f4f95a59441202c366c2cde954dd96337080dda
Jarl Saether <jarl_sondre@hotmail.com>
2024-10-11 16:14:31 +0200

    update slurm script

--- scaling-test/slurm.sh
+++ scaling-test/slurm.sh
@@ -7,11 +7,11 @@
 #SBATCH --account=intertwin
 #SBATCH --output=logs_slurm/horovod.out
 #SBATCH --error=logs_slurm/horovod.err
-#SBATCH --time=00:10:00
+#SBATCH --time=00:30:00
 
 # Resources allocation
 #SBATCH --partition=batch
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-gpu=4
 #SBATCH --exclusive
@@ -22,20 +22,29 @@ ml Stages/2024 GCC OpenMPI CUDA/12 MPI-settings/CUDA Python HDF5 PnetCDF libaio 
 PYTHON_VENV="../envAI_hdfml"
 source $PYTHON_VENV/bin/activate
 
-# srun --cpu-bind=none --ntasks-per-node=1 \
-# 	bash -c "torchrun \
-# 	--log_dir='logs_torchrun' \
-# 	--nnodes=$SLURM_NNODES \
-# 	--nproc_per_node=$SLURM_GPUS_PER_NODE \
-# 	--rdzv_id=$SLURM_JOB_ID \
-# 	--rdzv_conf=is_host=\$(((SLURM_NODEID)) && echo 0 || echo 1) \
-# 	--rdzv_backend=c10d \
-# 	--rdzv_endpoint='$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)':29500 \
-# 	profiler.py --epochs 5 --strategy deepspeed"
+EPOCHS=5
+TORCHRUN_STRATEGY="deepspeed"
 
+run_torchrun() {
+  srun --cpu-bind=none --ntasks-per-node=1 \
+      bash -c "torchrun \
+      --log_dir='logs_torchrun' \
+      --nnodes=$SLURM_NNODES \
+      --nproc_per_node=$SLURM_GPUS_PER_NODE \
+      --rdzv_id=$SLURM_JOB_ID \
+      --rdzv_backend=c10d \
+      --rdzv_endpoint='$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)':29500 \
+      profiler.py --epochs $EPOCHS --strategy $TORCHRUN_STRATEGY"
+}
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
-srun --cpu-bind=none --nodes=$SLURM_NNODES \
-	--ntasks-per-node=$SLURM_GPUS_PER_NODE \
-	--cpus-per-task=$SLURM_CPUS_PER_GPU \
-	python profiler.py --epochs 5 --strategy horovod
+run_horovod() {
+  export CUDA_VISIBLE_DEVICES="0,1,2,3"
+  srun --cpu-bind=none --nodes=$SLURM_NNODES \
+        --ntasks-per-node=$SLURM_GPUS_PER_NODE \
+        --cpus-per-task=$SLURM_CPUS_PER_GPU \
+        python profiler.py --epochs $EPOCHS --strategy horovod
+}
+
+# run_torchrun
+run_horovod
+
