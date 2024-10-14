8e747bcee10f12aa21f62bf728afba6ce23fd2ee
Jarl Saether <jarl_sondre@hotmail.com>
2024-10-14 16:49:08 +0200

    make comm profiler into decorator

--- scaling-test/profiler.py
+++ scaling-test/profiler.py
@@ -3,17 +3,13 @@ Adapted from: https://github.com/pytorch/examples/blob/main/mnist/main.py
 """
 
 import argparse
-import time
-from pathlib import Path
+import types
 
-import pandas as pd
-import torch
 import torchvision
 from dataset import imagenet_dataset
-from net import Net
-from torch.profiler import ProfilerActivity, profile
 from torch.utils.data import Subset
 
+from itwinai.components import profile_torch_trainer
 from itwinai.loggers import ConsoleLogger
 from itwinai.torch.config import TrainingConfiguration
 from itwinai.torch.trainer import TorchTrainer
@@ -42,8 +38,8 @@ def main():
     args = parser.parse_args()
     train_dataset = imagenet_dataset(args.data_dir)
 
-    # Only consider the first 1000 elements
-    indices = list(range(4000))
+    # Only consider a subset of the data
+    indices = list(range(1500))
     train_dataset = Subset(train_dataset, indices)
 
     model = torchvision.models.resnet152()
@@ -56,10 +52,10 @@ def main():
     )
     logger = ConsoleLogger()
 
-    profiler = profile(
-        activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU],
-        with_modules=True,
-    )
+    # profiler = profile(
+    #     activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU],
+    #     with_modules=True,
+    # )
 
     trainer = TorchTrainer(
         config=training_config,
@@ -67,43 +63,47 @@ def main():
         strategy=args.strategy,
         epochs=args.epochs,
         logger=logger,
+        validation_every=None,
     )
 
-    strategy = trainer.strategy
+    # strategy = trainer.strategy
+    trainer.execute = types.MethodType(profile_torch_trainer(trainer.execute), trainer)
 
-    profiler.start()
+    # profiler.start()
     trainer.execute(train_dataset)
-    profiler.stop()
+    # profiler.stop()
 
     # key_averages = profiler.key_averages(group_by_stack_n=0)
-    key_averages = profiler.key_averages()
-    print(key_averages.table(sort_by="cuda_time_total"))
-
-    profiling_data = []
-    for event in key_averages:
-        profiling_data.append(
-            {
-                "name": event.key,
-                "node_id": event.node_id,
-                "self_cpu_time_total": event.self_cpu_time_total,
-                "cpu_time_total": event.cpu_time_total,
-                "cpu_time_total_str": event.cpu_time_total_str,
-                "self_cuda_time_total": event.self_cuda_time_total,
-                "cuda_time_total": event.cuda_time_total,
-                "cuda_time_total_str": event.cuda_time_total_str,
-                "calls": event.count,
-            }
-        )
-    grank = strategy.global_rank()
-    num_gpus = strategy.global_world_size()
-    df = pd.DataFrame(profiling_data)
-
-    output_dir = Path("logs")
-    output_path = output_dir / f"profile_{args.strategy}_{num_gpus}_{grank}.csv"
-
-    print(f"Saving the dataframe to {output_path}", force=True)
-    df.to_csv(output_path, index=False)
-    strategy.clean_up()
+    # key_averages = profiler.key_averages()
+    # print(key_averages.table(sort_by="cuda_time_total"))
+    #
+    # profiling_data = []
+    # for event in key_averages:
+    #     profiling_data.append(
+    #         {
+    #             "name": event.key,
+    #             "node_id": event.node_id,
+    #             "self_cpu_time_total": event.self_cpu_time_total,
+    #             "cpu_time_total": event.cpu_time_total,
+    #             "cpu_time_total_str": event.cpu_time_total_str,
+    #             "self_cuda_time_total": event.self_cuda_time_total,
+    #             "cuda_time_total": event.cuda_time_total,
+    #             "cuda_time_total_str": event.cuda_time_total_str,
+    #             "calls": event.count,
+    #         }
+    #     )
+    # grank = strategy.global_rank()
+    # num_gpus = strategy.global_world_size()
+    # df = pd.DataFrame(profiling_data)
+    #
+    # output_dir = Path("profiling_logs")
+    # output_dir.mkdir(parents=True, exist_ok=True)
+    #
+    # output_path = output_dir / f"profile_{args.strategy}_{num_gpus}_{grank}.csv"
+    #
+    # print(f"Saving the dataframe to {output_path}", force=True)
+    # df.to_csv(output_path, index=False)
+    # strategy.clean_up()
 
 
 if __name__ == "__main__":
