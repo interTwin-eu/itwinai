e3176d93ea150b7d8d7f76e9815720d2ab5ddac4
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2024-10-18 17:22:40 +0200

    Update createEnvVega.sh

--- env-files/torch/createEnvVega.sh
+++ env-files/torch/createEnvVega.sh
@@ -13,13 +13,17 @@ ml Python
 ml CMake/3.24.3-GCCcore-11.3.0
 ml mpi4py
 ml OpenMPI
-ml CUDA/11.7
+#ml CUDA/11.7
+ml CUDA/12.3
 ml GCCcore/11.3.0
-ml NCCL/2.12.12-GCCcore-11.3.0-CUDA-11.7.0
-ml cuDNN
+#ml NCCL/2.12.12-GCCcore-11.3.0-CUDA-11.7.0
+ml NCCL
+ml cuDNN/8.9.7.29-CUDA-12.3.0
+
+# You should have CUDA 12.3 now
 
 
 # Create and install torch env
 export ENV_NAME=".venv-pytorch"
-export PIP_INDEX_TORCH_CUDA="https://download.pytorch.org/whl/cu118"
+export PIP_INDEX_TORCH_CUDA="https://download.pytorch.org/whl/cu121"
 bash env-files/torch/generic_torch.sh
