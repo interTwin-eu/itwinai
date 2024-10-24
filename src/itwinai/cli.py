0756c854a31fb5b5519652ec1c8fc9a6d65c455d
Matteo Bunino <matteo.bunino@gmail.com>
2024-10-24 13:51:35 +0200

    Refactor dockerfiles and improve tests

--- src/itwinai/cli.py
+++ src/itwinai/cli.py
@@ -291,6 +291,9 @@ def sanity_check(
         sanity_check_tensorflow()
     else:
         sanity_check_slim()
+        
+    if optional_deps is not None:
+        run_sanity_check(optional_deps)
 
     if optional_deps is not None:
         run_sanity_check(optional_deps)
