eacfacf70437e62e8947fd8dba4d6d377c2bee4b
Matteo Bunino <matteo.bunino@gmail.com>
2024-11-06 08:57:32 +0100

    Update CI

--- ci/run.sh
+++ ci/run.sh
@@ -1,4 +1,8 @@
 # Example of running dagger pipeline of build and local test for torch container
 dagger call \
-    build-torch --context=.. --dockerfile=../env-files/torch/Dockerfile \
-    test-torch
+    build-container --context=.. --dockerfile=../env-files/torch/Dockerfile \
+    test-local
+
+dagger call \
+    build-container --context=.. --dockerfile=../env-files/torch/Dockerfile \
+    publish
