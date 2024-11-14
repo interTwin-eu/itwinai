a98c8e04e1eeb17c661c93bf1c5bc71b57b65eed
Matteo Bunino <matteo.bunino@gmail.com>
2024-11-14 15:46:43 +0100

    Fix merge problems

--- src/itwinai/torch/distributed.py
+++ src/itwinai/torch/distributed.py
@@ -45,21 +45,6 @@ def check_initialized(method: Callable) -> Callable:
     return wrapper
 
 
-def check_initialized(method: Callable) -> Callable:
-    """Decorator for strategy methods to check whether the strategy
-    was correctly initialized before calling the method."""
-
-    @functools.wraps(method)
-    def wrapper(self: 'TorchDistributedStrategy', *args, **kwargs):
-        if not self.is_initialized:
-            raise UninitializedStrategyError((
-                f"{self.__class__.__name__} has not been initialized. "
-                "Use the init method."))
-        return method(self, *args, **kwargs)
-
-    return wrapper
-
-
 class TorchDistributedStrategy(DistributedStrategy):
     """Abstract class to define the distributed backend methods for
     PyTorch models.
@@ -322,9 +307,7 @@ class TorchDistributedStrategy(DistributedStrategy):
                     shuffle=shuffle,
                 )
             elif not isinstance(sampler, DistributedSampler):
-                raise RuntimeError(
-                    "User-provided sampler must implement DistributedSampler."
-                )
+                raise RuntimeError("User-provided sampler must implement DistributedSampler.")
         # shuffle and batch_sampler must be unset
         return DataLoader(
             dataset=dataset,
@@ -701,9 +684,7 @@ class DeepSpeedStrategy(TorchDistributedStrategy):
         dist.gather_object(obj, dst=dst_rank)
 
     @check_initialized
-    def gather(
-        self, tensor: torch.Tensor, dst_rank: int = 0
-    ) -> Optional[List[torch.Tensor]]:
+    def gather(self, tensor: torch.Tensor, dst_rank: int = 0) -> Optional[List[torch.Tensor]]:
         """Gathers a tensor from the whole group in a list
         (to all workers).
 
@@ -882,9 +863,7 @@ class HorovodStrategy(TorchDistributedStrategy):
             return result
 
     @check_initialized
-    def gather(
-        self, tensor: torch.Tensor, dst_rank: int = 0
-    ) -> Optional[List[torch.Tensor]]:
+    def gather(self, tensor: torch.Tensor, dst_rank: int = 0) -> Optional[List[torch.Tensor]]:
         """Gathers a tensor from the whole group in a list
         (to all workers). Under the hood it relies on allgather as gather is
         not supported by Horovod.
