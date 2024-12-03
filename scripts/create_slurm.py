28cdefe9d02c8f3237c96651bb88bb7b8a372f4f
Jarl Sondre Sæther <60541573+jarlsondre@users.noreply.github.com>
2024-12-03 16:33:45 +0100

    add eurac slurm script for testing

--- scripts/create_slurm.py
+++ scripts/create_slurm.py
@@ -6,12 +6,16 @@ def remove_indentation_from_multiline_string(multiline_string: str) -> str:
     return "\n".join([line.lstrip() for line in multiline_string.split("\n")])
 
 
-def get_srun_command(distributed_strategy: str, training_command: str) -> str:
+def get_srun_command(
+    distributed_strategy: str,
+    training_command: str,
+    torch_log_dir: str = "logs_torchrun",
+) -> str:
     if distributed_strategy in ["ddp", "deepspeed"]:
         main_command = rf"""
         srun --cpu-bind=none --ntasks-per-node=1 \
         bash -c "torchrun \
-        --log_dir='logs_torchrun' \
+        --log_dir='{torch_log_dir}' \
         --nnodes=$SLURM_NNODES \
         --nproc_per_node=$SLURM_GPUS_PER_NODE \
         --rdzv_id=$SLURM_JOB_ID \
@@ -22,6 +26,7 @@ def get_srun_command(distributed_strategy: str, training_command: str) -> str:
     """
     elif distributed_strategy == "horovod":
         main_command = rf"""
+	    export CUDA_VISIBLE_DEVICES="0,1,2,3"
         srun --cpu-bind=none \
             --ntasks-per-node=$SLURM_GPUS_PER_NODE \
             --cpus-per-task=$SLURM_CPUS_PER_GPU \
@@ -47,6 +52,7 @@ def slurm_template(
     num_nodes: int,
     num_tasks_per_node: int,
     gpus_per_node: int,
+    cpus_per_gpu: int,
     setup_command: str,
     main_command: str,
 ) -> str:
@@ -66,6 +72,7 @@ def slurm_template(
         #SBATCH --nodes={num_nodes}
         #SBATCH --ntasks-per-node={num_tasks_per_node}
         #SBATCH --gpus-per-node={gpus_per_node}
+        #SBATCH --cpus-per-gpu={cpus_per_gpu}
         #SBATCH --exclusive
         
         {setup_command}
@@ -77,6 +84,7 @@ def slurm_template(
 
 
 def main():
+    # SLURM specific settings
     job_name = "my_test_job"
     account = "intertwin"
     time = "01:00:00"
@@ -86,7 +94,9 @@ def main():
     num_nodes = 1
     num_tasks_per_node = 1
     gpus_per_node = 4
+    cpus_per_gpu = 4
 
+    # Setting up the environment
     setup_command = """
         ml Stages/2024 GCC OpenMPI CUDA/12 MPI-settings/CUDA Python HDF5 PnetCDF libaio mpi4py
         source .venv/bin/activate
@@ -94,7 +104,11 @@ def main():
     setup_command = setup_command.strip()
     setup_command = remove_indentation_from_multiline_string(setup_command)
 
-    main_command = get_srun_command("ddp", "python main.py")
+    # Main training command, e.g. srun
+    torch_log_dir = "logs_torchrun"
+    main_command = get_srun_command(
+        "ddp", "python main.py", torch_log_dir=torch_log_dir
+    )
     template = slurm_template(
         job_name=job_name,
         account=account,
@@ -105,6 +119,7 @@ def main():
         num_nodes=num_nodes,
         num_tasks_per_node=num_tasks_per_node,
         gpus_per_node=gpus_per_node,
+        cpus_per_gpu=cpus_per_gpu,
         setup_command=setup_command,
         main_command=main_command,
     )
---
+++ scripts/eurac_slurm.py
@@ -0,0 +1,87 @@
+from create_slurm import (
+    slurm_template,
+    get_srun_command,
+    remove_indentation_from_multiline_string,
+)
+
+
+def get_eurac_setup_command(venv: str, cpus_per_gpu: int):
+    if cpus_per_gpu > 0:
+        omp_num_threads = "$SLURM_CPUS_PER_GPU"
+    else:
+        omp_num_threads = "1"
+
+    setup_command = rf"""
+        ml Stages/2024 GCC OpenMPI CUDA/12 MPI-settings/CUDA Python/3.11.3 HDF5 PnetCDF libaio mpi4py
+        source {venv}/bin/activate
+        export OMP_NUM_THREADS={omp_num_threads}
+    """
+
+    setup_command = setup_command.strip()
+    return remove_indentation_from_multiline_string(setup_command)
+
+
+def get_eurac_training_command(
+    config_file: str, pipe_key: str, distributed_strategy: str
+):
+    training_command = rf"""
+    $(which itwinai) exec-pipeline \
+        --config {config_file} \
+        --pipe-key {pipe_key} \
+        -o strategy={distributed_strategy} 
+    """
+    training_command = training_command.strip()
+    return remove_indentation_from_multiline_string(training_command)
+
+
+def main():
+    job_name = "my_test_job"
+    account = "intertwin"
+    time = "01:00:00"
+    partition = "develbooster"
+    std_out = "job.out"
+    err_out = "job.err"
+    num_nodes = 1
+    num_tasks_per_node = 1
+    gpus_per_node = 4
+    cpus_per_gpu = 4
+
+    distributed_strategy = "ddp"
+    venv = ".venv"
+
+    config_file = "config.yaml"
+    pipe_key = "rnn_training_pipeline"
+
+    dist_strats = ["ddp", "deepspeed", "horovod"]
+
+    for distributed_strategy in dist_strats: 
+        training_command = get_eurac_training_command(
+            config_file=config_file,
+            pipe_key=pipe_key,
+            distributed_strategy=distributed_strategy,
+        )
+        main_command = get_srun_command(
+            distributed_strategy=distributed_strategy, training_command=training_command
+        )
+        setup_command = get_eurac_setup_command(venv=venv, cpus_per_gpu=cpus_per_gpu)
+        template = slurm_template(
+            job_name=job_name,
+            account=account,
+            time=time,
+            partition=partition,
+            std_out=std_out,
+            err_out=err_out,
+            num_nodes=num_nodes,
+            num_tasks_per_node=num_tasks_per_node,
+            gpus_per_node=gpus_per_node,
+            cpus_per_gpu=cpus_per_gpu,
+            setup_command=setup_command,
+            main_command=main_command,
+        )
+        print("#"*20)
+        print(template)
+        print("#"*20)
+
+
+if __name__ == "__main__":
+    main()
