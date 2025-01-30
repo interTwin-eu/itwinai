5044f40b7d49efea1c50520718c8f46dc6798cc2
iacopoff <iacopo.ff@gmail.com>
2025-01-30 15:21:31 +0100

    fix typing and default list args in data.py

--- use-cases/eurac/data.py
+++ use-cases/eurac/data.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Any
+from typing import Optional, Tuple, Any, List, Dict
 import xarray as xr
 from itwinai.components import DataSplitter, monitor_exec
 from hython.datasets import get_dataset
@@ -21,24 +21,30 @@ class RNNDatasetGetterAndPreprocessor(DataSplitter):
         experiment_run: str,
         data_source: dict,
         work_dir: str,
-        dynamic_inputs: list[str] = None,
-        static_inputs: list[str] = None,
-        target_variables: list[str] = None,
-        scaling_static_range: dict = None,
-        mask_variables: list[str] = None,
-        train_temporal_range: list[str] = ["", ""],
-        valid_temporal_range: list[str] = ["", ""],
-        train_downsampler: dict = None,
-        valid_downsampler: dict = None,
+        dynamic_inputs: List[str] | None = None,
+        static_inputs: List[str] | None = None,
+        target_variables: List[str] | None = None,
+        scaling_static_range: Dict | None = None,
+        mask_variables: List[str] | None = None,
+        train_temporal_range: List[str] = None,
+        valid_temporal_range: List[str] = None,
+        train_downsampler: Dict | None = None,
+        valid_downsampler: Dict | None = None,
         # == calibration ==
-        data_dynamic_inputs: str = None,
-        data_static_inputs: str = None,
-        data_target_variables: str = None,
-        data_target_mask: str = None,
-        min_sample_target: int = None,
-        seq_length: int = None,
+        data_dynamic_inputs: str | None = None,
+        data_static_inputs: str | None = None,
+        data_target_variables: str | None = None,
+        data_target_mask: str | None = None,
+        min_sample_target: int | None = None,
+        seq_length: int | None = None,
         # == training ==
     ) -> None:
+        
+        if train_temporal_range is None:
+            train_temporal_range = ["", ""]
+        if valid_temporal_range is None:
+            valid_temporal_range = ["", ""]
+
         self.save_parameters(**self.locals2params(locals()))
 
         self.cfg = deepcopy(self.locals2params(locals()))
