702339061726d7c8c3c2d1447ee9b01ecdf2a77f
Anna Lappe <annalappe@gmx.de>
2025-02-06 13:01:57 +0100

    Making tests run to see where it fails

--- use-cases/3dgan/trainer.py
+++ use-cases/3dgan/trainer.py
@@ -85,6 +85,7 @@ class Lightning3DGANTrainer(Trainer):
             local_yaml_path = os.path.join(tmp_dir, "pl-conf.yaml")
             with open(local_yaml_path, "w") as outfile:
                 print(f"Config: {self.conf}")
+                print(f"Type of config: {type(self.conf)}")
                 yaml.dump(self.conf, outfile, default_flow_style=False)
             logger.log(local_yaml_path, "lightning-config", kind="artifact")
 
