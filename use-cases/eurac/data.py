4076d75b1a1ba40536b69a74bac7ad0d867f864d
iacopoff <iacopo.ff@gmail.com>
2025-03-06 08:58:06 +0100

    clean calib config

--- use-cases/eurac/data.py
+++ use-cases/eurac/data.py
@@ -15,7 +15,6 @@ class RNNDatasetGetterAndPreprocessor(DataSplitter):
         # == common ==
         hython_trainer: str,
         dataset: str,
-        downsampling_temporal_dynamic: bool,
         data_lazy_load: bool,
         scaling_variant: str,
         scaling_use_cached: bool,
@@ -32,10 +31,10 @@ class RNNDatasetGetterAndPreprocessor(DataSplitter):
         head_model_inputs: List[str] | None = None,
         train_temporal_range: List[str] = None,
         valid_temporal_range: List[str] = None,
-        cal_temporal_range: List[str] = None,
         train_downsampler: AbstractDownSampler | None = None,
         valid_downsampler: AbstractDownSampler | None = None,
         cal_downsampler: dict = None,
+        downsampling_temporal_dynamic: bool | None = None,
         # == calibration ==
         min_sample_target: int | None = None,
         seq_length: int | None = None,
@@ -52,12 +51,7 @@ class RNNDatasetGetterAndPreprocessor(DataSplitter):
 
         scaler = Scaler(cfg, cfg.scaling_use_cached)
 
-        period = "train"
-        istrain = True
-        if "cal" in cfg.hython_trainer:
-            period = "cal"
-
-        train_dataset = get_dataset(cfg.dataset)(cfg, scaler, istrain, period)
+        train_dataset = get_dataset(cfg.dataset)(cfg, scaler, True, "train")
 
         val_dataset = get_dataset(cfg.dataset)(cfg, scaler, False, "valid")
 
