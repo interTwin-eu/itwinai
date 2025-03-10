1d9ef61de60b3a43075111d6431ebc4e02062085
Rakesh Sarma <126173968+r-sarma@users.noreply.github.com>
2025-03-10 15:18:32 +0100

    Addition of generic distributed trainer and pipeline run

--- use-cases/lattice-qcd/startscript.sh
+++ use-cases/lattice-qcd/startscript.sh
@@ -12,7 +12,6 @@
 #SBATCH --nodes=2
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --threads-per-core=1
 #SBATCH --cpus-per-task=32
 #SBATCH --exclusive
 
@@ -23,7 +22,8 @@ export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
 ml Stages/2024 GCC OpenMPI CUDA/12 MPI-settings/CUDA Python HDF5 PnetCDF libaio mpi4py
 
 # Activate the virtual environment
-source envAI_juwels/bin/activate
+#source envAI_juwels/bin/activate
+source /p/project1/intertwin/rakesh/T6.5-AI-and-ML/latticeqcd/Use_Case_T4.1_normflow/itwinai/itwinai/envAI_juwels/bin/activate
 
 export MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
 if [ "$SYSTEMNAME" = juwelsbooster ] \
@@ -47,7 +47,7 @@ export NCCL_SOCKET_IFNAME=ib0
 # Prevent Gloo not being able to communicate.
 export GLOO_SOCKET_IFNAME=ib0
 
-srun --cpu-bind=none bash -c "torchrun \
+srun --cpu-bind=none --ntasks-per-node=1 bash -c "torchrun \
     --log_dir='logs' \
     --nnodes=$SLURM_NNODES \
     --nproc_per_node=$SLURM_GPUS_PER_NODE \
@@ -55,4 +55,4 @@ srun --cpu-bind=none bash -c "torchrun \
     --rdzv_conf=is_host=\$(((SLURM_NODEID)) && echo 0 || echo 1) \
     --rdzv_backend=c10d \
     --rdzv_endpoint='$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)'i:54123 \
-    train.py"
+   $(which itwinai) exec-pipeline --config pipeline.yaml"
