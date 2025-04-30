279e993cf776b4ed89826e6bc3d1650c4b01d700
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2025-04-30 10:55:43 +0300

    update scripts

--- use-cases/mnist/torch/runall.lumi.sh
+++ use-cases/mnist/torch/runall.lumi.sh
@@ -9,9 +9,8 @@
 # - Matteo Bunino <matteo.bunino@cern.ch> - CERN
 # --------------------------------------------------------------------------------------
 
-# export CONTAINER_PATH="../../../../itwinai-rocm-slim.sif"
-
-export CONTAINER_PATH="../../../../container_test.sif"
+# SIF: oras://registry.egi.eu/dev.intertwin.eu/itwinai-dev:lumi-itwinai-pytorch-rocm-6.1.3-python-3.12-pytorch-v2.4.1
+export CONTAINER_PATH="/project/project_465001592/itwinai-containers/lumi-itwinai-pytorch-rocm-6.1.3-python-3.12-pytorch-v2.4.1.sif"
 
 
 # Clear SLURM logs (*.out and *.err files)
@@ -20,15 +19,14 @@ mkdir -p logs_slurm logs_torchrun
 
 export HYDRA_FULL_ERROR=1
 
-# DDP itwinai
-DIST_MODE="ddp"
-RUN_NAME="ddp-itwinai"
-TRAINING_CMD="itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp"
-sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$RUN_NAME-n$N" \
-    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
-    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
-    slurm.lumi.sh
+# # DDP itwinai
+# DIST_MODE="ddp"
+# RUN_NAME="ddp-itwinai"
+# sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$RUN_NAME-n$N" \
+#     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+#     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+#     slurm.lumi.sh
 
 # # DeepSpeed itwinai
 # DIST_MODE="deepspeed"
@@ -40,15 +38,15 @@ sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$T
 #     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
 #     slurm.lumi.sh
 
-# # Horovod itwinai
-# DIST_MODE="horovod"
-# RUN_NAME="horovod-itwinai"
-# TRAINING_CMD="itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd"
-# sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$RUN_NAME-n$N" \
-#     --output="logs_slurm/job-$RUN_NAME-n$N.out" \
-#     --error="logs_slurm/job-$RUN_NAME-n$N.err" \
-#     slurm.lumi.sh
+# Horovod itwinai
+DIST_MODE="horovod"
+RUN_NAME="horovod-itwinai"
+TRAINING_CMD="itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",RUN_NAME="$RUN_NAME",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$RUN_NAME-n$N" \
+    --output="logs_slurm/job-$RUN_NAME-n$N.out" \
+    --error="logs_slurm/job-$RUN_NAME-n$N.err" \
+    slurm.lumi.sh
 
 
 # ### GAN training ###
