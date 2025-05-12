3eddcde78e44bbdc556136d0e39954e5e418f51f
lineick <linusmaxeickhoff@gmail.com>
2025-05-12 14:24:00 +0200

    linting

--- src/itwinai/torch/monitoring/backend.py
+++ src/itwinai/torch/monitoring/backend.py
@@ -6,16 +6,18 @@
 # Credit:
 # - Linus Eickhoff <linus.maximilian.eickhoff@cern.ch> - CERN
 # --------------------------------------------------------------------------------------
-from types import ModuleType
-from typing import Tuple, List, Any
 import logging
+from types import ModuleType
+from typing import Any, List, Tuple
 
 py_logger = logging.getLogger(__name__)
 Backend = Tuple[str, List[Any], ModuleType]  # ("nvidia" | "amd"), handles, module
 
+
 def init_backend() -> Backend:
     try:
         import pynvml as nv
+
         nv.nvmlInit()  # will raise if no NVIDIA driver
         handles = [nv.nvmlDeviceGetHandleByIndex(i) for i in range(nv.nvmlDeviceGetCount())]
         py_logger.info("Monitoring: NVIDIA backend set up")
@@ -25,6 +27,7 @@ def init_backend() -> Backend:
 
     try:
         import amdsmi
+
         amdsmi.amdsmi_init()  # raises on non-AMD nodes
         handles = amdsmi.amdsmi_get_processor_handles()
         py_logger.info("Monitoring: AMD backend set up")
@@ -33,5 +36,3 @@ def init_backend() -> Backend:
         py_logger.warning("Monitoring: AMD backend could not be set up")
 
     raise RuntimeError("Monitoring: No supported GPU backend found")
-
-
