5d26113f43afc594edb0e5d02ad74e724da10576
Jarl Sondre Sæther <60541573+jarlsondre@users.noreply.github.com>
2025-07-03 09:52:24 +0200

    remove incorrect tests

--- tests/components/test_config_parsing.py
+++ tests/components/test_config_parsing.py
@@ -85,19 +85,7 @@ def test_instantiate_and_execute_pipeline(pipe_key, temp_yaml_files, monkeypatch
         mock_trainer_init.assert_called_once_with(lr=0.001, batch_size=32, name="my-trainer")
         mock_trainer_exec.assert_called_once()
 
-
-@pytest.mark.parametrize(
-    "pipe_key",
-    [
-        "my-list-pipeline",
-        "my-dict-pipeline",
-        "some.field.my-nested-pipeline",
-        "my-interpolation-pipeline",
-    ],
-)
-def test_step_selection(pipe_key, temp_yaml_files, monkeypatch):
-    """Test that exec_pipeline can correctly select steps via cli arguments and instantiate
-    and execute only the selected steps from the pipeline."""
+def test_step_selection_list_pipeline(temp_yaml_files, monkeypatch):
     with (
         patch(
             "itwinai.tests.dummy_components.FakePreproc.__init__", return_value=None
@@ -112,6 +100,7 @@ def test_step_selection(pipe_key, temp_yaml_files, monkeypatch):
             "itwinai.tests.dummy_components.FakeTrainer.execute", return_value=None
         ) as mock_trainer_exec,
     ):
+        pipe_key="my-list-pipeline"
         path = temp_yaml_files[pipe_key]
         args = [
             "exec_pipeline",
@@ -125,12 +114,46 @@ def test_step_selection(pipe_key, temp_yaml_files, monkeypatch):
 
         exec_pipeline()
 
-        mock_preproc_init.assert_called_once_with(max_items=33, name="my-preproc")
-        mock_preproc_exec.assert_called_once()
+        mock_trainer_init.assert_called_once_with(lr=0.001, batch_size=32, name="my-trainer")
+        mock_trainer_exec.assert_called_once()
 
-        mock_trainer_init.assert_not_called()
-        mock_trainer_exec.assert_not_called()
+        mock_preproc_init.assert_not_called()
+        mock_preproc_exec.assert_not_called()
 
+def test_step_selection_dict_pipeline(temp_yaml_files, monkeypatch):
+    with (
+        patch(
+            "itwinai.tests.dummy_components.FakePreproc.__init__", return_value=None
+        ) as mock_preproc_init,
+        patch(
+            "itwinai.tests.dummy_components.FakePreproc.execute", return_value=None
+        ) as mock_preproc_exec,
+        patch(
+            "itwinai.tests.dummy_components.FakeTrainer.__init__", return_value=None
+        ) as mock_trainer_init,
+        patch(
+            "itwinai.tests.dummy_components.FakeTrainer.execute", return_value=None
+        ) as mock_trainer_exec,
+    ):
+        pipe_key="my-dict-pipeline"
+        path = temp_yaml_files[pipe_key]
+        args = [
+            "exec_pipeline",
+            f"--config-dir={path.parent}",
+            f"--config-path={path.parent}",
+            f"--config-name={path.stem}",
+            f"+pipe_key={pipe_key}",
+            "+pipe_steps=[train-step]",
+        ]
+        monkeypatch.setattr(sys, "argv", args)
+
+        exec_pipeline()
+
+        mock_trainer_init.assert_called_once_with(lr=0.001, batch_size=32, name="my-trainer")
+        mock_trainer_exec.assert_called_once()
+
+        mock_preproc_init.assert_not_called()
+        mock_preproc_exec.assert_not_called()
 
 
 def test_dynamic_override_interpolation_list_pipeline(temp_yaml_files, monkeypatch):
@@ -138,7 +161,6 @@ def test_dynamic_override_interpolation_list_pipeline(temp_yaml_files, monkeypat
     and execute the steps with the updated arguments. This tests the list pipeline with
     interpolation keys."""
     with (
-        pytest.raises(SystemExit) as sys_exit,
         patch(
             "itwinai.tests.dummy_components.FakeTrainer.__init__", return_value=None
         ) as mock_trainer_init,
@@ -150,7 +172,8 @@ def test_dynamic_override_interpolation_list_pipeline(temp_yaml_files, monkeypat
             "exec_pipeline",
             f"--config-path={path.parent}",
             f"--config-dir={path.parent}",
-            f"pipe_key={pipe_key}",
+            f"--config-name={path.stem}",
+            f"+pipe_key={pipe_key}",
             "lr=0.005",
             "my-interpolation-pipeline.steps.1.batch_size=64",
         ]
@@ -158,29 +181,30 @@ def test_dynamic_override_interpolation_list_pipeline(temp_yaml_files, monkeypat
         exec_pipeline()
 
         mock_trainer_init.assert_called_once_with(lr=0.005, batch_size=64, name="my-trainer")
-    assert sys_exit.value.code == 0, "Pipeline should run through without fail"
 
 
 def test_dynamic_override_cli_dict_pipeline(temp_yaml_files, monkeypatch):
     """Test that exec_pipeline can correctly set keys via cli override and instantiate
     and execute the steps with the updated arguments. This tests the dictionary pipeline."""
     with (
-        pytest.raises(SystemExit) as sys_exit,
         patch(
             "itwinai.tests.dummy_components.FakePreproc.__init__", return_value=None
         ) as mock_preproc_init,
     ):
+        pipe_key = 'my-dict-pipeline'
+        path = temp_yaml_files[pipe_key]
         args = [
             "exec_pipeline",
-            f"--config-path={temp_yaml_files['my-dict-pipeline']}",
-            "pipe_key=my-dict-pipeline",
+            f"--config-path={path.parent}",
+            f"--config-dir={path.parent}",
+            f"--config-name={path.stem}",
+            f"+pipe_key={pipe_key}",
             "my-dict-pipeline.steps.preproc-step.name=new-preproc",
         ]
         monkeypatch.setattr(sys, "argv", args)
         exec_pipeline()
 
         mock_preproc_init.assert_called_once_with(max_items=33, name="new-preproc")
-    assert sys_exit.value.code == 0, "Pipeline should run through without fail"
 
 
 def test_invalid_step_selection(temp_yaml_files, monkeypatch):
