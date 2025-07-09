384c9425a75f182e76c8fda69f0d60eb6c91cde0
Matteo Bunino <48362942+matbun@users.noreply.github.com>
2025-07-09 10:38:08 +0200

    Update distributed.py

--- src/itwinai/distributed.py
+++ src/itwinai/distributed.py
@@ -66,7 +66,7 @@ def ray_cluster_is_running() -> bool:
     except subprocess.CalledProcessError:
         # If the command fails, the cluster is not running
         py_logger.debug(
-            "Ray was checking for the existance of a Ray cluster trying to "
+            "Ray was checking for the existance of a Ray cluster by trying to "
             "connect to it, but could not do it. This is not a problem if you "
             "are not plannig to connect to a Ray cluster."
         )
