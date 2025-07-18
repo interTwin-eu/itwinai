42e8aa93bb91cae787c1634f3379c0e0db5becd2
lineick <75929322+lineick@users.noreply.github.com>
2025-07-18 15:48:15 +0200

    fixed scripts

--- use-cases/mnist/torch/runall.jsc.sh
+++ use-cases/mnist/torch/runall.jsc.sh
@@ -30,36 +30,36 @@ TOT_GPUS=$(( NNODES * NGPUS_PER_NODE ))
 
 # DDP itwinai
 DIST_MODE="ddp"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
-sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.jsc.sh
 
 # DeepSpeed itwinai
 DIST_MODE="deepspeed"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
-sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.jsc.sh
 
 # Horovod itwinai
 DIST_MODE="horovod"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
-sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.jsc.sh
@@ -69,22 +69,22 @@ sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAIN
 
 # # DDP itwinai
 # DIST_MODE="ddp"
-# run_id="$DIST_MODE-gan-${1:-itwinai}"
-# TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_id=$run_id +pipe_key=training_pipeline_gan ray_num_workers=$TOT_GPUS"
-# sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$run_id-n$N" \
-#     --output="logs_slurm/job-$run_id-n$N.out" \
-#     --error="logs_slurm/job-$run_id-n$N.err" \
+# run_name="$DIST_MODE-gan-${1:-itwinai}"
+# TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_name=$run_name +pipe_key=training_pipeline_gan ray_num_workers=$TOT_GPUS"
+# sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$run_name-n$N" \
+#     --output="logs_slurm/job-$run_name-n$N.out" \
+#     --error="logs_slurm/job-$run_name-n$N.err" \
 #     slurm.jsc.sh
 
 # # DeepSpeed itwinai
 # DIST_MODE="deepspeed"
-# run_id="$DIST_MODE-gan-${1:-itwinai}"
-# TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_id=$run_id +pipe_key=training_pipeline_gan ray_num_workers=$TOT_GPUS"
-# sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$run_id-n$N" \
-#     --output="logs_slurm/job-$run_id-n$N.out" \
-#     --error="logs_slurm/job-$run_id-n$N.err" \
+# run_name="$DIST_MODE-gan-${1:-itwinai}"
+# TRAINING_CMD="$PYTHON_VENV/bin/itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_name=$run_name +pipe_key=training_pipeline_gan ray_num_workers=$TOT_GPUS"
+# sbatch --export=ALL,DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$run_name-n$N" \
+#     --output="logs_slurm/job-$run_name-n$N.out" \
+#     --error="logs_slurm/job-$run_name-n$N.err" \
 #     slurm.jsc.sh
 
 # NOTE: GAN with Horovod does not work!
--- use-cases/mnist/torch/runall.lumi.sh
+++ use-cases/mnist/torch/runall.lumi.sh
@@ -30,37 +30,37 @@ TOT_GPUS=$(( NNODES * NGPUS_PER_NODE ))
 
 # DDP itwinai
 DIST_MODE="ddp"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
 # TRAINING_CMD="itwinai check-distributed-cluster --platform amd --launcher ray"
-sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.lumi.sh
 
 # DeepSpeed itwinai
 DIST_MODE="deepspeed"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
-sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.lumi.sh
 
 # Horovod itwinai
 DIST_MODE="horovod"
-run_id="$DIST_MODE-${1:-itwinai}"
-TRAINING_CMD="itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd run_id=$run_id +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
-sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-    --job-name="$run_id-n$TOT_GPUS" \
-    --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-    --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+run_name="$DIST_MODE-${1:-itwinai}"
+TRAINING_CMD="itwinai exec-pipeline strategy=horovod checkpoints_location=checkpoints_hvd run_name=$run_name +pipe_key=training_pipeline ray_num_workers=$TOT_GPUS"
+sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+    --job-name="$run_name-n$TOT_GPUS" \
+    --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+    --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
     --nodes=$NNODES \
     --gpus-per-node=$NGPUS_PER_NODE \
     slurm.lumi.sh
@@ -69,24 +69,24 @@ sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_
 
 # # DDP itwinai
 # DIST_MODE="ddp"
-# run_id="$DIST_MODE-gan-${1:-itwinai}"
-# TRAINING_CMD="itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_id=$run_id +pipe_key=training_pipeline_gan"
-# sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$run_id-n$TOT_GPUS" \
-#     --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-#     --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+# run_name="$DIST_MODE-gan-${1:-itwinai}"
+# TRAINING_CMD="itwinai exec-pipeline strategy=ddp checkpoints_location=checkpoints_ddp run_name=$run_name +pipe_key=training_pipeline_gan"
+# sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$run_name-n$TOT_GPUS" \
+#     --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+#     --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
 #     --nodes=$NNODES \
 #     --gpus-per-node=$NGPUS_PER_NODE \
 #     slurm.lumi.sh
 
 # # DeepSpeed itwinai
 # DIST_MODE="deepspeed"
-# run_id="$DIST_MODE-gan-${1:-itwinai}"
-# TRAINING_CMD="itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_id=$run_id +pipe_key=training_pipeline_gan"
-# sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_id="$run_id",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
-#     --job-name="$run_id-n$TOT_GPUS" \
-#     --output="logs_slurm/job-$run_id-n$TOT_GPUS.out" \
-#     --error="logs_slurm/job-$run_id-n$TOT_GPUS.err" \
+# run_name="$DIST_MODE-gan-${1:-itwinai}"
+# TRAINING_CMD="itwinai exec-pipeline strategy=deepspeed checkpoints_location=checkpoints_deepspeed run_name=$run_name +pipe_key=training_pipeline_gan"
+# sbatch --export=ALL,CONTAINER_PATH="$CONTAINER_PATH",DIST_MODE="$DIST_MODE",run_name="$run_name",TRAINING_CMD="$TRAINING_CMD",PYTHON_VENV="$PYTHON_VENV" \
+#     --job-name="$run_name-n$TOT_GPUS" \
+#     --output="logs_slurm/job-$run_name-n$TOT_GPUS.out" \
+#     --error="logs_slurm/job-$run_name-n$TOT_GPUS.err" \
 #     --nodes=$NNODES \
 #     --gpus-per-node=$NGPUS_PER_NODE \
 #     slurm.lumi.sh
