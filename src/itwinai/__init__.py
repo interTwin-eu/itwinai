fcccc823ba518c5c67bd2c2293b24e1787886d6d
lineick <75929322+lineick@users.noreply.github.com>
2025-07-30 16:45:05 +0200

    jarls fix

--- src/itwinai/__init__.py
+++ src/itwinai/__init__.py
@@ -32,7 +32,7 @@ logger.setLevel(getattr(logging, log_level.upper()))
 
 # Prevent duplicate handlers
 if not logger.hasHandlers():
-    handler = logging.StreamHandler()
+    handler = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter("[%(levelname)s] %(name)s:%(lineno)d - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
